3cb997c9d99ee5b9490dd350b3053ce4eeeeafec
Roman <roma.buchuk@gmail.com>
2015-06-02 13:32:26 +0300

    clone DB demo 1

--- routes/importData.js
+++ routes/importData.js
@@ -24,12 +24,31 @@ module.exports = function (models) {
         }
     });
 
-    function queryBuilder(table){
+    function queryBuilder(table) {
         return 'SELECT * FROM ' + table;
+    }
+
+    function hrImporter(tasks) {
+        var departmentShema = tasks[0];
+        var jobPositionShema = tasks[1];
+        var employeeShema = tasks[2];
+
+        function importDepartment(departmentShema, callback) {
+            var query = queryBuilder(departmentShema.table);
+
+            handler.importData(query, callback);
+        }
+
+        function departmentImporter(callback) {
+            importDepartment(departmentShema, callback);
+        }
+
+        return [departmentImporter];
     }
 
     router.post('/', function (req, res, next) {
         var tasksLength = tasks.length || 0;
+        var waterfallTasks = hrImporter(tasks);
         /*handler.importData("SELECT * FROM Employee", function(err, employees){
          if(err){
          return next(err);
@@ -38,11 +57,8 @@ module.exports = function (models) {
          res.status(200).send(employees);
          });*/
 
-        for (var i = tasksLength - 1; i >= 0; i--) {
-            tasks[i] = handler.importData.bind(tasks[i]);
-        }
 
-        async.waterfall(tasks, function (task, callback) {
+        async.waterfall(waterfallTasks, function (task, callback) {
             var collection = task.collection;
             var Schema = mongoose.Schemas[collection];
             var Model = models.get(req.session.lastDb, collection, Schema);
