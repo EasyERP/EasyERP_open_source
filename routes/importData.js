bbc4349733a480ab76c47e4a86af74563d1abb2a
soundstorm <soundstorm.mail@gmail.com>
2015-06-03 16:37:19 +0300

    importer

--- routes/importData.js
+++ routes/importData.js
@@ -29,20 +29,18 @@ module.exports = function (models) {
        var targetObject =  _.find(field, function(fieldValue){
            return fieldValue.value.toLowerCase() === columnName.toString().toLowerCase();
        });
-
         return targetObject.fieldValue;
-    }
+    };
 
     function queryBuilder(table) {
         return 'SELECT * FROM ' + table;
-    }
+    };
 
-    function hrImporter(req, tasks) {
+    function salesImporter(req, tasks) {
         var departmentShema = tasks[0];
-        var jobPositionShema = tasks[1];
+        var projectShema = tasks[1];
         var employeeShema = tasks[2];
-        var projectShema = tasks[3];
-        var customerSchema = tasks[4];
+        var customerSchema = tasks[3];
         var ownerId = req.session ? req.session.uId : null;
 
         function importCustomer(customerSchema, seriesCb) {
@@ -109,7 +107,134 @@ module.exports = function (models) {
 
                 seriesCb(null, 'Complete')
             });
-        }
+        };
+
+        function importProject(projectShema, seriesCb) {
+            var query = queryBuilder(projectShema.table);
+            var waterfallTasks;
+
+            function getData(callback) {
+                handler.importData(query, callback);
+            }
+
+            function saverProject(fetchedArray, callback) {
+                var collection = projectShema.collection;
+                var customerCollection = customerSchema.collection;
+                var employeeCollection = employeeShema.collection;
+                var Schema = mongoose.Schemas[collection];
+                var CustomerSchema = mongoose.Schemas[customerCollection];
+                var EmployeeShema = mongoose.Schema[employeeCollection];
+                var Model = models.get(req.session.lastDb, collection, Schema);
+                var Customer = models.get(req.session.lastDb, customerCollection, CustomerSchema);
+                var Employee = models.get(req.session.lastDb, employeeCollection, EmployeeShema);
+                var model;
+
+                var mongooseFields = Object.keys(projectShema.aliases);
+
+                var q = async.queue(function (fetchedProject, cb) {
+                    var objectToSave = {};
+                    var customerQuery;
+                    var employeeQuery;
+
+                    for (var i = mongooseFields.length - 1; i >= 0; i--) {
+                        var key = mongooseFields[i];
+                        var msSqlKey = projectShema.aliases[key];
+
+                        if (msSqlKey in projectShema.comparator) {
+                            fetchedProject[msSqlKey] = comparator(fetchedProject[msSqlKey], projectShema.comparator[msSqlKey]);
+                        }
+
+                        objectToSave[key] = fetchedProject[msSqlKey];
+                        objectToSave.createdBy = {
+                            user: ownerId
+                        };
+                        objectToSave.editedBy = {
+                            user: ownerId
+                        }
+                    }
+
+                    if (fetchedProject) {
+                        employeeQuery = {
+                            ID: fetchedProject['Assigned']
+                        };
+                        customerQuery = {
+                            ID: fetchedProject['Company']
+                        };
+
+                        function customerFinder (callback) {
+                            Customer.findOne(customerQuery, {_id: 1}, function (err, customer) {
+                                if (err) {
+                                    return callback(err);
+                                }
+                                callback(null, customer);
+                            });
+                        };
+
+                        function employeeFinder (callback) {
+                            Employee.findOne(employeeQuery, {_id: 1}, function (err, employee) {
+                                if (err) {
+                                    return callback(err);
+                                }
+                                callback(null, employee);
+                            });
+                        }
+
+                        async.parallel({
+                            customerResult: customerFinder,
+                            employeeResult: employeeFinder
+                        }, function(err, result) {
+                            objectToSave.customer = result.customerResult._id;
+                            objectToSave.employee = result.employeeResult._id;
+                            model = new Model(objectToSave);
+                            model.save(cb);
+                        });
+                    }
+                }, 100);
+
+                q.drain = function () {
+                    callback(null, 'done');
+                };
+
+                async.each(fetchedArray, function (fetchedCustomer) {
+                    q.push(fetchedCustomer, function () {
+                        console.log('finished ' + fetchedCustomer.ID);
+                    });
+                }, function (err) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, 'Completed');
+                })
+            }
+
+            waterfallTasks = [getData, saverProject];
+
+            async.waterfall(waterfallTasks, function (err, result) {
+                if (err) {
+                    seriesCb(err);
+                }
+
+                seriesCb(null, 'Complete')
+            });
+        };
+
+        function projectImporter(callback) {
+            importProject(projectShema, callback);
+        };
+
+        function customerImporter(callback) {
+            importCustomer(customerSchema, callback);
+        };
+
+        return [customerImporter, projectImporter];
+    };
+
+    function hrImporter(req, tasks) {
+        var departmentShema = tasks[0];
+        var jobPositionShema = tasks[1];
+        var employeeShema = tasks[2];
+        var ownerId = req.session ? req.session.uId : null;
 
         function importDepartment(departmentShema, seriesCb) {
             var query = queryBuilder(departmentShema.table);
@@ -262,116 +387,6 @@ module.exports = function (models) {
             });
         }
 
-        function importProject(projectShema, seriesCb) {
-            var query = queryBuilder(projectShema.table);
-            var waterfallTasks;
-
-            function getData(callback) {
-                handler.importData(query, callback);
-            }
-
-            function saverProject(fetchedArray, callback) {
-                var collection = projectShema.collection;
-                var customerCollection = customerSchema.collection;
-                var employeeCollection = employeeShema.collection;
-                var Schema = mongoose.Schemas[collection];
-                var CustomerSchema = mongoose.Schemas[customerCollection];
-                var EmployeeShema = mongoose.Schema[employeeCollection];
-                var Model = models.get(req.session.lastDb, collection, Schema);
-                var Customer = models.get(req.session.lastDb, customerCollection, CustomerSchema);
-                var Employee = models.get(req.session.lastDb, employeeCollection, EmployeeShema);
-                var model;
-
-                var mongooseFields = Object.keys(projectShema.aliases);
-
-                var q = async.queue(function (fetchedProject, cb) {
-                    var objectToSave = {};
-                    var customerQuery;
-                    var employeeQuery;
-
-                    for (var i = mongooseFields.length - 1; i >= 0; i--) {
-                        var key = mongooseFields[i];
-                        var msSqlKey = jobPositionShema.aliases[key];
-
-                        if (msSqlKey in jobPositionShema.comparator) {
-                            fetchedProject[msSqlKey] = comparator(fetchedProject[msSqlKey], projectShema.comparator[msSqlKey]);
-                        }
-
-                        objectToSave[key] = fetchedProject[msSqlKey];
-                        objectToSave.createdBy = {
-                            user: ownerId
-                        };
-                        objectToSave.editedBy = {
-                            user: ownerId
-                        }
-                    }
-
-                    if (fetchedProject) {
-                        employeeQuery = {
-                            ID: fetchedProject['Assigned']
-                        };
-                        customerQuery = {
-                            ID: fetchedProject['Company']
-                        };
-
-                        async.parallel({
-                            customerFinder: function(cbresult) {
-                                Customer.findOne(customerQuery, {_id: 1}, function (err, customer) {
-                                    if (err) {
-                                        return cbresult(err);
-                                    }
-                                    cbresult(null, customer);
-                                });
-                            },
-                            employeeFinder: function(cbresult) {
-                                Employee.findOne(employeeQuery, {_id: 1}, function (err, employee) {
-                                    if (err) {
-                                        return cbresult(err);
-                                    }
-                                    cbresult(null, employee);
-                                });
-                            }
-                        }, function(err, result) {
-                            objectToSave.customer = result.customerFinder._id;
-                            objectToSave.employee = result.employeeFinder._id;
-                            model = new Model(objectToSave);
-                            model.save(cb);
-                        });
-                    }
-                }, 100);
-
-                q.drain = function () {
-                    callback(null, 'done');
-                };
-
-                async.each(fetchedArray, function (fetchedCustomer) {
-                    q.push(fetchedCustomer, function () {
-                        console.log('finished ' + fetchedCustomer.ID);
-                    });
-                }, function (err) {
-                    if (err) {
-                        return callback(err);
-                    }
-
-                    callback(null, 'Completed');
-                })
-            }
-
-            waterfallTasks = [getData, saverProject];
-
-            async.waterfall(waterfallTasks, function (err, result) {
-                if (err) {
-                    seriesCb(err);
-                }
-
-                seriesCb(null, 'Complete')
-            });
-        }
-
-        function projectImporter(callback) {
-            importProject(projectShema, callback);
-        }
-
         function jobPositionImporter(callback) {
             importJobPosition(jobPositionShema, callback);
         }
@@ -380,11 +395,13 @@ module.exports = function (models) {
             importDepartment(departmentShema, callback);
         }
 
-        return [departmentImporter, jobPositionImporter, importCustomer, projectImporter];
+        return [departmentImporter, jobPositionImporter];
     }
 
     router.post('/', function (req, res, next) {
-        var seriesTasks = hrImporter(req, tasks);
+        var hrTasks = hrImporter(req, tasks);
+        var salesTasks = salesImporter(req, tasks);
+        var seriesTasks = hrTasks.concat(salesTasks);
         /*handler.importData("SELECT * FROM Employee", function(err, employees){
          if(err){
          return next(err);
