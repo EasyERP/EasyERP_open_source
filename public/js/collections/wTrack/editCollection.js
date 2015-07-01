f3693e5a3a7b765824b533bcfcda51a348664d78
Roman <roma.buchuk@gmail.com>
2015-07-01 20:19:15 +0300

    manual merged code2

--- public/js/collections/wTrack/editCollection.js
+++ public/js/collections/wTrack/editCollection.js
@@ -2,69 +2,64 @@
         './filterCollection'
     ],
     function (ParrantCollection) {
-        var EditableCoolecton = ParrantCollection.extend({
+        var EditableColection = ParrantCollection.extend({
 
             initialize: function(){
                 this.on( "change", this.change, this);
             },
 
-        save: function (changes) {
-            var self = this;
-            var model;
-            var models = [];
-            var newModel;
-            var modelObject;
-            var options;
-            var saveObject;
-            var updatedOptions;
-            var syncObject;
+            save: function(changedValues){
+                var self = this;
+                var model;
+                var models = [];
+                var newModel;
+                var modelObject;
+                var syncObject = {
+                    trigger: this.trigger,
+                    url: this.url,
+                    toJSON: function () {
+                        return models;
+                    }
+                };
 
-            syncObject = {
-                trigger: this.trigger,
-                url: this.url,
-                toJSON: function () {
-                    return models;
+                var saveObject = {
+                    trigger: this.trigger,
+                    url: this.url,
+                    toJSON: function () {
+                        return newModel;
+                    }
+                };
+
+                var options = {
+                    success: function (model, resp, xhr) {
+                        self.trigger('saved', model);
+                    }
+                };
+                var updatedOptions = {
+                    success: function (model, resp, xhr) {
+                        self.trigger('updated');
+                    }
+                };
+
+                for (var i = this.models.length - 1; i >=0; i--){
+                    model = this.models[i];
+
+                    if(model && model.id && model.hasChanged()){
+                        modelObject = model.changed;
+                        modelObject._id = model.id;
+                        models.push(modelObject);
+                    } else if (model && !model.id){
+                        newModel = model.changed;
+                        newModel._id =  model.id;
+                        Backbone.sync("create", saveObject, options);
+                    }
                 }
-            };
 
-            saveObject = {
-                trigger: this.trigger,
-                url: this.url,
-                toJSON: function () {
-                    return newModel;
+                if(models.length) {
+                    Backbone.sync("patch", syncObject, updatedOptions);
                 }
-            };
-
-            options = {
-                success: function (model) {
-                    self.trigger('saved', model);
-                }
-            };
-
-            updatedOptions = {
-                success: function (model, resp, xhr) {
-                    self.trigger('updated');
-                }
-            };
-
-            for (var i = this.models.length - 1; i >=0; i--){
-                model = this.models[i];
-
-                if(model && model.id && model.hasChanged()){
-                    modelObject = model.changed;
-                    modelObject._id = model.id;
-                    models.push(modelObject);
-                } else if (model && !model.id){
-                    newModel = model.changed;
-                    newModel._id =  model.id;
-                    Backbone.sync("create", saveObject, options);
-                }
-            };
-
-            if(models.length) {
-                Backbone.sync("patch", syncObject, updatedOptions);
             }
-        }
+        });
+
+        return EditableColection;
     });
-    return EditableCollection;
-});
