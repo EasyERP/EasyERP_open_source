0353a8d100a929157723d8b060dc9168e5af74ad
Roman <roma.buchuk@gmail.com>
2015-07-03 10:55:59 +0300

    bonusTypes

--- models/bonusType.js
+++ models/bonusType.js
@@ -4,9 +4,7 @@
 module.exports = (function () {
     var mongoose = require('mongoose');
     var ObjectId = mongoose.Schema.Types.ObjectId;
-    var bonusTypeSchema;
-
-    bonusTypeSchema = mongoose.Schema({
+    var bonusTypeSchema = mongoose.Schema({
         name: {type: String},
         department: {
             _id: {type: ObjectId, ref: 'Department', default: null},
