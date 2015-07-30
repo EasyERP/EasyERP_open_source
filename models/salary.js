6d2918c80d0c36d3fdc4da7910008df733dc2896
soundstorm <soundstorm.mail@gmail.com>
2015-07-30 10:32:36 +0300

    salary

--- models/salary.js
+++ models/salary.js
@@ -27,12 +27,6 @@ module.exports = (function () {
             onCash: Number,
             onCard: Number,
             total: Number
-        },
-        whoCanRW: {type: String, enum: ['owner', 'group', 'everyOne'], default: 'everyOne'},
-        groups: {
-            owner: {type: ObjectId, ref: 'Users', default: null},
-            users: [{type: ObjectId, ref: 'Users', default: null}],
-            group: [{type: ObjectId, ref: 'Department', default: null}]
         }
     }, {collection: 'Salary'});
 
--- models/salaryCash.js
+++ models/salaryCash.js
@@ -44,13 +44,7 @@ module.exports = (function () {
                 onCard: Number,
                 total: Number
             }
-        }],
-        whoCanRW: {type: String, enum: ['owner', 'group', 'everyOne'], default: 'everyOne'},
-        groups: {
-            owner: {type: ObjectId, ref: 'Users', default: null},
-            users: [{type: ObjectId, ref: 'Users', default: null}],
-            group: [{type: ObjectId, ref: 'Department', default: null}]
-        }
+        }]
     }, {collection: 'SalaryCash'});
 
     mongoose.model('SalaryCash', salaryCashSchema);
