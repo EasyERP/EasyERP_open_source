64a992a304ac182adf2520327ab4aa35a7e62c33
PikinerLiliya <liliya.pikiner@gmail.com>
2015-07-30 16:58:48 +0300

    filters fix

--- public/js/views/Leads/list/ListView.js
+++ public/js/views/Leads/list/ListView.js
@@ -427,7 +427,10 @@ define([
                         });
                         FilterView.bind('defaultFilter', function () {
                             showList = _.pluck(self.stages, '_id');
-                            self.showFilteredPage(showList)
+                            self.showFilteredPage(showList);
+                            $(".saveFilterButton").hide();
+                            $(".clearFilterButton").hide();
+                            $(".removeFilterButton").show();
                         });
                         // Filter custom event listen ------end
 
