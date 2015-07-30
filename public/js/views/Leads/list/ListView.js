2f3dfbfaf58896c4b0cd64b0e270640707fb3fc9
YuraDerevenko <jedprankster7@gmail.com>
2015-07-30 16:42:51 +0300

    condition for leads, opportunities

--- public/js/views/Leads/list/ListView.js
+++ public/js/views/Leads/list/ListView.js
@@ -173,6 +173,7 @@ define([
                 var isConverted = null;
                 var self = this;
                 var checkedElements = $('.drop-down-filter input:checkbox:checked');
+                var condition = this.$el.find('.conditionAND > input')[0];
                 var chosen = this.$el.find('.chosen');
                 var showList;
 
@@ -183,7 +184,11 @@ define([
                 this.newCollection = false;
                 this.filter = {};
                 this.filter['isConverted'] = isConverted;
+                this.filter['condition'] = 'and';
 
+                if  (!condition.checked) {
+                    self.filter['condition'] = 'or';
+                }
                 /*if (checkedElements.length && checkedElements.attr('id') !== 'defaultFilter') {
                     showList = $('.drop-down-filter input:checkbox:checked').map(function() {
                         return this.value
