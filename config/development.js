abefeccafae331a1a50e8bc0f6822f7462870e3e
soundstorm <soundstorm.mail@gmail.com>
2015-10-01 16:55:12 +0300

    redis - localhost

--- config/development.js
+++ config/development.js
@@ -5,5 +5,5 @@
 process.env.MAIN_SUB_DOMAIN = 'live';
 process.env.HOST = 'http://localhost:8089/';
 process.env.SOCKET_DB_PORT = 6379;
-process.env.SOCKET_DB_HOST = '192.168.88.250';
+process.env.SOCKET_DB_HOST = 'localhost';
 process.env.SOCKET_DB = 9;
