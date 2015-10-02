39bdd5f5d472d1fdb1433ee60a216689d22f5d20
Roman <roma.buchuk@gmail.com>
2015-10-02 10:09:04 +0300

    weTracks change employee 2

--- public/js/views/Projects/projectInfo/wTracks/generateWTrack.js
+++ public/js/views/Projects/projectInfo/wTracks/generateWTrack.js
@@ -12,13 +12,13 @@ define(["text!templates/Projects/projectInfo/wTracks/generate.html",
 			responseObj              : {},
 
 			events: {
-				"click .newSelectList li:not(.miniStylePagination)": "chooseOption",
-				"click .current-selected"                          : "showNewSelect",
-				"click .newSelectList li.miniStylePagination": "notHide",
+				"click .newSelectList li:not(.miniStylePagination)"               : "chooseOption",
+				"click .current-selected"                                         : "showNewSelect",
+				"click .newSelectList li.miniStylePagination"                     : "notHide",
 				"click .newSelectList li.miniStylePagination .next:not(.disabled)": "nextSelect",
 				"click .newSelectList li.miniStylePagination .prev:not(.disabled)": "prevSelect",
-				"click #addNewEmployeeRow"                         : "addNewEmployeeRow",
-				"click"                                            : "hideNewSelect"
+				"click #addNewEmployeeRow"                                        : "addNewEmployeeRow",
+				"click"                                                           : "hideNewSelect"
 			},
 
 			initialize: function (options) {
@@ -34,31 +34,27 @@ define(["text!templates/Projects/projectInfo/wTracks/generate.html",
 				e.stopPropagation();
 
 				var target = $(e.target);
-				var wTrackPerEmployeeContainer = this.$el.find('#wTrackItemsHolder');
+				//var wTrackPerEmployeeContainer = this.$el.find('#wTrackItemsHolder');
 				var parrent = target.closest('tbody');
 				var parrentRow = parrent.find('.productItem').last();
 				var rowId = parrentRow.attr("data-id");
 				var trEll = parrent.find('tr.productItem');
-				var elem;
-
-				/*if (rowId === undefined || rowId !== 'false') {
-					if (!trEll.length) {
-						return parrent.prepend(_.template(ProductInputContent));
-					}
-					$(trEll[trEll.length - 1]).after(_.template(ProductInputContent));
-				}
-
-				this.default = wTrackPerEmployeeContainer.find('#rawTable');*/
-
-				elem = this.wTrackPerEmployeeTemplate({
-					year    : 2015,
-					month   : 10,
-					week    : 40
+				var elem = this.wTrackPerEmployeeTemplate({
+					year : 2015,
+					month: 10,
+					week : 40
 				});
 
-				parrent.append(
-					elem
-				);
+
+
+				if (rowId === undefined || rowId !== 'false') {
+					if (!trEll.length) {
+						return parrent.prepend(elem);
+					}
+					$(trEll[trEll.length - 1]).after(elem);
+				}
+
+				//this.default = wTrackPerEmployeeContainer.find('#rawTable');
 			},
 
 			generateItems: function () {
@@ -87,89 +83,20 @@ define(["text!templates/Projects/projectInfo/wTracks/generate.html",
 				var target = $(e.target);
 				var targetElement = target.parents("td");
 				var tr = target.parents("tr");
-				var modelId = tr.attr('data-id');
 				var id = target.attr("id");
 				var attr = targetElement.attr("id") || targetElement.data("content");
 				var elementType = '#' + attr;
-				var projectManager;
-				var assignedContainer;
-				var project;
-				var employee;
-				var department;
-				var changedAttr;
-				var wTrackId = tr.data('id');
-				var week;
-				var year;
 
 				var element = _.find(this.responseObj[elementType], function (el) {
 					return el._id === id;
 				});
 
-				/*var editWtrackModel = this.editCollection.get(modelId) ? this.editCollection.get(modelId) : this.collection.get(modelId);
+				tr.attr('data-id', id);
 
-				if (!this.changedModels[modelId]) {
-					if (!editWtrackModel.id) {
-						this.changedModels[modelId] = editWtrackModel.attributes;
-					} else {
-						this.changedModels[modelId] = {};
-					}
-				}
-
-				changedAttr = this.changedModels[modelId];*/
-
-				if (elementType === '#project') {
-
-					projectManager = element.projectmanager.name;
-					assignedContainer = tr.find('[data-content="assigned"]');
-					assignedContainer.text(projectManager);
-					targetElement.attr('data-id', id);
-
-					tr.find('[data-content="workflow"]').text(element.workflow.name);
-					tr.find('[data-content="customer"]').text(element.customer.name);
-
-					project = _.clone(editWtrackModel.get('project'));
-					project._id = element._id;
-					project.projectName = element.projectName;
-					project.workflow._id = element.workflow._id;
-					project.workflow.name = element.workflow.name;
-					project.customer._id = element.customer._id;
-					project.customer.name = element.customer.name;
-
-					project.projectmanager.name = element.projectmanager.name;
-					project.projectmanager._id = element.projectmanager._id;
-
-					//changedAttr.project = project;
-
-				} else if (elementType === '#employee') {
+				if (elementType === '#employee') {
 					tr.find('[data-content="department"]').text(element.department.name);
 
-					/*employee = _.clone(editWtrackModel.get('employee'));
-					department = _.clone(editWtrackModel.get('department'));
-
-					employee._id = element._id;
-					employee.name = target.text();
-
-					department._id = element.department._id;
-					department.departmentName = element.department.name;
-
-					changedAttr.employee = employee;
-					changedAttr.department = department;*/
-
 					tr.find('[data-content="department"]').removeClass('errorContent');
-				} else if (elementType === '#department') {
-					department = _.clone(editWtrackModel.get('department'));
-					department._id = element._id;
-					department.departmentName = element.departmentName;
-
-					//changedAttr.department = department;
-				} else if (elementType === '#week') {
-					week = $(e.target).text();
-
-					//changedAttr.week = week;
-				} else if (elementType === '#year') {
-					year = $(e.target).text();
-
-					//changedAttr.year = year;
 				}
 
 				targetElement.removeClass('errorContent');
@@ -177,7 +104,6 @@ define(["text!templates/Projects/projectInfo/wTracks/generate.html",
 				targetElement.text(target.text());
 
 				this.hideNewSelect();
-				//this.setEditable(targetElement);
 
 				return false;
 			},
