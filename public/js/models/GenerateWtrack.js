739281c180583a43939706c91bc3266c6720c223
soundstorm <soundstorm.mail@gmail.com>
2015-10-06 16:20:26 +0300

    model + collection

---
+++ public/js/models/GenerateWtrack.js
@@ -0,0 +1,46 @@
+define(['Validation', 'common', 'moment'], function (Validation, common, moment) {
+    var wTrackModel = Backbone.Model.extend({
+        idAttribute: "_id",
+
+        defaults: {
+            startDate: new Date(),
+            endDate: '',
+            hours: '',
+            project: {
+                projectName: '',
+                workflow: {},
+                customer: {},
+                projectmanager: {}
+            },
+            employee: {},
+            department: {},
+            weekDefault: {
+                1: 8,
+                2: 8,
+                3: 8,
+                4: 8,
+                5: 8,
+                6: 0,
+                7: 0
+            },
+            revenue: 120
+        },
+        validate: function(attrs){
+            var errors = [];
+
+            Validation.checkNumberField(errors, true, attrs.weekDefault['1'], "First day value");
+            Validation.checkNumberField(errors, true, attrs.weekDefault['2'], "Second day value");
+            Validation.checkNumberField(errors, true, attrs.weekDefault['3'], "Third day value");
+            Validation.checkNumberField(errors, true, attrs.weekDefault['4'], "Fourth day value");
+            Validation.checkNumberField(errors, true, attrs.weekDefault['5'], "Fifth day value");
+            Validation.checkNumberField(errors, true, attrs.weekDefault['6'], "Sixth day value");
+            Validation.checkNumberField(errors, true, attrs.weekDefault['7'], "Seventh day value");
+            Validation.checkMoneyField(errors, false, attrs.revenue, "Revenue");
+
+            if(errors.length > 0)
+                return errors;
+        },
+    });
+
+    return wTrackModel;
+});
