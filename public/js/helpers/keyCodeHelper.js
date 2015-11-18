76ea84dad585780a5c8a1a9ec06d33fbfd1a00a8
MichaelKapustey <michaelkapustey@gmail.com>
2015-11-18 11:10:33 +0200

    refactor

--- public/js/helpers/keyCodeHelper.js
+++ public/js/helpers/keyCodeHelper.js
@@ -1,7 +1,7 @@
 define([], function () {
     var isDigit = function (keyCode) {
-        return (keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105);
-
+        return (keyCode >= 48 && keyCode <= 57) //0-9 number
+            || (keyCode >= 96 && keyCode <= 105); //0-9 numpad
     };
 
     var isBackspace = function (keyCode) {
