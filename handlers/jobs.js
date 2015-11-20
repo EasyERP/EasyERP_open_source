8e432abb92f84071c8446b95c0198f4ab3998cf3
romashka50 <roma.buchuk@gmail.com>
2015-11-20 13:14:28 +0200

    jobs review

--- handlers/jobs.js
+++ handlers/jobs.js
@@ -72,21 +72,21 @@ var Jobs = function (models, event) {
     },
 
         this.getForDD = function (req, res, next) {
-        var pId = req.query.projectId;
-        var query = models.get(req.session.lastDb, 'jobs', JobsSchema);
+            var pId = req.query.projectId;
+            var query = models.get(req.session.lastDb, 'jobs', JobsSchema);
 
-        query.find({type: "Empty", project: objectId(pId)}, {
-            name: 1,
-            _id: 1,
-            "budget.budgetTotal.revenueSum": 1
-        }, function (err, jobs) {
-            if (err) {
-                return next(err);
-            }
-            res.status(200).send(jobs);
+            query.find({type: "Empty", project: objectId(pId)}, {
+                name                           : 1,
+                _id                            : 1,
+                "budget.budgetTotal.revenueSum": 1
+            }, function (err, jobs) {
+                if (err) {
+                    return next(err);
+                }
+                res.status(200).send(jobs);
 
-        });
-    },
+            });
+        },
 
         this.remove = function (req, res, next) {
             var JobsModel = models.get(req.session.lastDb, 'jobs', JobsSchema);
@@ -96,12 +96,15 @@ var Jobs = function (models, event) {
             var id = data._id;
 
             JobsModel.findByIdAndRemove(id, function (err, result) {
+                var jobId;
+                var projectId;
+
                 if (err) {
                     return next(err);
                 }
 
-                var jobId = result.get('_id');
-                var projectId = result.get('project');
+                jobId = result.get('_id');
+                projectId = result.get('project');
 
                 wTrack.find({"jobs._id": jobId}, function (err, result) {
                     if (err) {
@@ -116,7 +119,7 @@ var Jobs = function (models, event) {
                         });
                     }, function () {
                         console.log('wTracks removed');
-                        if (projectId){
+                        if (projectId) {
                             event.emit('updateProjectDetails', {req: req, _id: projectId});
                         }
                     });
@@ -135,28 +138,33 @@ var Jobs = function (models, event) {
 
             var data = req.body;
             var id = data._id;
-            if (id) {
-                var query;
-                var updatewTracks = false;
+            var query;
+            var updatewTracks = false;
+            var products;
+            var type;
 
+            if (id) {
                 if (data.workflowId) {
                     query = {workflow: {_id: data.workflowId, name: data.workflowName}};
                 } else if (data.name) {
                     query = {name: data.name};
                     updatewTracks = true;
-                } else if (data.type){
+                } else if (data.type) {
                     query = {type: data.type};
                 }
 
                 delete data._id;
 
                 JobsModel.findByIdAndUpdate(id, query, {new: true}, function (err, result) {
+                    var jobId;
+                    var jobName;
+
                     if (err) {
                         return next(err);
                     }
 
-                    var jobId = result.get('_id');
-                    var jobName = result.get('name');
+                    jobId = result.get('_id');
+                    jobName = result.get('name');
 
                     if (updatewTracks) {
                         wTrack.update({"jobs._id": jobId}, {$set: {"jobs.name": jobName}}, {multi: true}, function (err, result) {
@@ -169,11 +177,10 @@ var Jobs = function (models, event) {
                     }
 
                     res.status(200).send(result)
-                })
+                });
             } else if (data.products && data.products.length) {
-                var products = JSON.parse(data.products);
-                var type = data.type;
-
+                products = JSON.parse(data.products);
+                type = data.type;
 
                 async.each(products, function (product, cb) {
 
@@ -185,21 +192,15 @@ var Jobs = function (models, event) {
                         project = result.get('project');
 
                         cb();
-
                     });
 
-                }, function(){
-                    if (project){
+                }, function () {
+                    if (project) {
                         event.emit('fetchJobsCollection', {project: project});
                     }
                 });
-
             }
-
-
         }
-
-
 };
 
 module.exports = Jobs;
