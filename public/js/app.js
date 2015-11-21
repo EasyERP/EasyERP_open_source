3f2c77e2174f3f49b4c35695d2e6c53188125156
romashka50 <roma.buchuk@gmail.com>
2015-11-21 12:57:53 +0200

    app refactor_2

--- public/js/app.js
+++ public/js/app.js
@@ -10,34 +10,34 @@ define([
         var appRouter = new Router();
 
         var opts = {
-            lines: 17 // The number of lines to draw
-            , length: 56 // The length of each line
-            , width: 14 // The line thickness
-            , radius: 82 // The radius of the inner circle
-            , scale: 0.75 // Scales overall size of the spinner
-            , corners: 1 // Corner roundness (0..1)
-            , color: '#000' // #rgb or #rrggbb or array of colors
-            , opacity: 0.25 // Opacity of the lines
-            , rotate: 68 // The rotation offset
-            , direction: 1 // 1: clockwise, -1: counterclockwise
-            , speed: 1.6 // Rounds per second
-            , trail: 89 // Afterglow percentage
-            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-            , zIndex: 2000000000 // The z-index (defaults to 2000000000)
-            , className: 'spinner' // The CSS class to assign to the spinner
-            , top: '50%' // Top position relative to parent
-            , left: '50%' // Left position relative to parent
-            , shadow: true // Whether to render a shadow
-            , hwaccel: false // Whether to use hardware acceleration
-            , position: 'absolute' // Element positioning
+            lines    : 17, // The number of lines to draw
+            length   : 56, // The length of each line
+            width    : 14, // The line thickness
+            radius   : 82, // The radius of the inner circle
+            scale    : 0.75, // Scales overall size of the spinner
+            corners  : 1, // Corner roundness (0..1)
+            color    : '#000', // #rgb or #rrggbb or array of colors
+            opacity  : 0.25, // Opacity of the lines
+            rotate   : 68, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            speed    : 1.6, // Rounds per second
+            trail    : 89, // Afterglow percentage
+            fps      : 20, // Frames per second when using setTimeout() as a fallback for CSS
+            zIndex   : 2000000000, // The z-index (defaults to 2000000000)
+            className: 'spinner', // The CSS class to assign to the spinner
+            top      : '50%', // Top position relative to parent
+            left     : '50%', // Left position relative to parent
+            shadow   : true, // Whether to render a shadow
+            hwaccel  : false, // Whether to use hardware acceleration
+            position : 'absolute' // Element positioning
         };
         var target = $('#loading');
         var spinner = new Spinner(opts).spin(target);
 
-        $(document).ajaxStart(function(){
+        $(document).ajaxStart(function () {
             $(target).fadeIn();
         });
-        $(document).ajaxComplete(function(){
+        $(document).ajaxComplete(function () {
             $(target).fadeOut();
         });
 
@@ -72,7 +72,7 @@ define([
             draggable: true,
             autoOpen : true,
             width    : 700,
-            appendTo: '#dialogContainer',
+            appendTo : '#dialogContainer',
             create   : function (event, ui) {
                 var win = $(window);
                 var dialog = $(event.target).parent(".ui-dialog");
