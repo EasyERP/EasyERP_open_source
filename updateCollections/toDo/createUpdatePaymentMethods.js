0be64b9cb348e55642f94ab402bab39d007f003e
soundstorm <soundstorm.mail@gmail.com>
2015-11-30 16:46:11 +0200

    findAndModify -> update

--- updateCollections/toDo/createUpdatePaymentMethods.js
+++ updateCollections/toDo/createUpdatePaymentMethods.js
@@ -15,7 +15,7 @@ MongoClient.connect(url, function (err, db) {
     PaymentMethod.insert({'name':'26005536599700', 'account':'26005536599700','currency':'UAH','bank':'Ukrsibbank', 'owner':'ThinkMobiles'})
     PaymentMethod.insert({'name':'26000479199400', 'account':'26000479199400','currency':'USD','bank':'Ukrsibbank', 'owner':'YourTradingSystems'})
     PaymentMethod.insert({'name':'26000479199400', 'account':'26000479199400','currency':'UAH','bank':'Ukrsibbank', 'owner':'YourTradingSystems'})
-    PaymentMethod.findAndModify({query: {'_id': id}, update: {$set: {'name':'Payoneer ', 'account':'Payoneer ','currency':'USD','bank':'', 'owner':'Payoneer '}}})
+    PaymentMethod.update({'_id': id}, {$set: {'name':'Payoneer ', 'account':'Payoneer ','currency':'USD','bank':'', 'owner':'Payoneer '}})
     PaymentMethod.insert({'name':'Unicreditbank', 'account':'Unicreditbank','currency':'USD','bank':'', 'owner':'Unicreditbank'})
     PaymentMethod.insert({'name':'CASH USD', 'account':'CASH USD','currency':'USD','bank':'', 'owner':'CASH USD'})
     PaymentMethod.insert({'name':'CASH UAH', 'account':'CASH UAH','currency':'UAH','bank':'', 'owner':'CASH UAH'})
