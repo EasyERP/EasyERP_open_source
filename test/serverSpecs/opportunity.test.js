1d92f0381316489103931474aef847f3823f37b9
Denis Saranyuk <denissaranyuk@gmail.com>
2016-02-05 14:11:17 +0200

    testUser

--- test/serverSpecs/opportunity.test.js
+++ test/serverSpecs/opportunity.test.js
@@ -9,60 +9,102 @@ var aggent;
 describe("Opportunity Specs", function () {
     'use strict';
     var id;
-    before(function (done) {
-        aggent = request.agent(url);
 
-        aggent
-            .post('users/login')
-            .send({
-                login: 'admin',
-                pass : '1q2w3eQWE',
-                dbId : 'production'
-            })
-            .expect(200, done);
+    describe('Opportunity with correct user', function(){
+        before(function (done) {
+            aggent = request.agent(url);
+
+            aggent
+                .post('users/login')
+                .send({
+                    login: 'admin',
+                    pass : '1q2w3eQWE',
+                    dbId : 'production'
+                })
+                .expect(200, done);
+        });
+
+        after(function(done){
+            aggent
+                .get('logout')
+                .expect(302, done);
+        });
+
+        it('should create Opportunity', function(done){
+            var body = {
+                name: "Subject"
+            };
+
+            aggent
+                .post('opportunities')
+                .send(body)
+                .expect(201)
+                .end(function (err, res) {
+                    var body = res.body;
+
+                    if (err) {
+                        return done(err);
+                    }
+
+                    expect(body)
+                        .to.be.instanceOf(Object);
+                    expect(body)
+                        .to.have.property('success');
+
+                    id = body.id;
+
+                    done();
+                });
+        });
+
+        it('should fail create Opportunity', function(done){
+            var body = {
+
+            };
+
+            aggent
+                .post('opportunities')
+                .send(body)
+                .expect(404, done);
+        });
+
+        it("should remove opportunity", function (done) {
+            aggent
+                .delete('opportunities/' + id)
+                .expect(200, done);
+        });
     });
 
-    it('should create Opportunity', function(done){
-        var body = {
-            name: "Subject",
-        };
 
-        aggent
-            .post('opportunities')
-            .send(body)
-            .expect(201)
-            .end(function (err, res) {
-                var body = res.body;
+    describe('Opportunity with fail user', function(){
+        before(function (done) {
+            aggent = request.agent(url);
 
-                if (err) {
-                    return done(err);
-                }
+            aggent
+                .post('users/login')
+                .send({
+                    login: 'testUser',
+                    pass : 'qwerty',
+                    dbId : 'production'
+                })
+                .expect(200, done);
+        });
 
-                expect(body)
-                    .to.be.instanceOf(Object);
-                expect(body)
-                    .to.have.property('success');
+        after(function(done){
+            aggent
+                .get('logout')
+                .expect(302, done);
+        });
 
-                id = body.id;
+        it('should fail create Opportunity', function(done){
+            var body = {
+                name: "Subject"
+            };
 
-                done();
-            });
-    });
-
-    it('should fail create Opportunity', function(done){
-        var body = {
-
-        };
-
-        aggent
-            .post('opportunities')
-            .send(body)
-            .expect(404, done);
-    });
-
-    it("should remove opportunity", function (done) {
-        aggent
-            .delete('opportunities/' + id)
-            .expect(200, done);
+            aggent
+                .post('opportunities')
+                .send(body)
+                .expect(403, done);
+        });
     });
 });
