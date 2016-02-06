3d93cf796a4cd34e6ad7591a08f205d61ae2ae0c
PikinerLiliya <liliya.pikiner@gmail.com>
2016-02-06 11:56:05 +0200

    persons topBar

--- public/js/views/Persons/TopBarView.js
+++ public/js/views/Persons/TopBarView.js
@@ -1,94 +1,28 @@
 define([
-        'Backbone',
-        'jQuery',
         'Underscore',
+        'views/topBarViewBase',
         'text!templates/Persons/TopBarTemplate.html',
         'text!templates/Notes/importTemplate.html',
-        'views/Notes/AttachView',
-        'custom',
-        'common'
+        'custom'
     ],
-    function (Backbone, $, _, ContentTopBarTemplate, importTemplate, AttachView, Custom, Common) {
+    function (_, BaseView, ContentTopBarTemplate, importTemplate, Custom) {
         'use strict';
-        var TopBarView = Backbone.View.extend({
-            el         : '#top-bar',
-            contentType: "Persons",
-            template   : _.template(ContentTopBarTemplate),
-
-            events: {
-                "click a.changeContentView"     : 'changeContentViewType',
-                "click #top-bar-deleteBtn"      : "deleteEvent",
-                "click #top-bar-editBtn"        : "editEvent",
-                "click #top-bar-createBtn"      : "createEvent",
-                "click #top-bar-exportBtn"      : "export",
-                "click #top-bar-exportToCsvBtn" : "exportToCsv",
-                "click #top-bar-exportToXlsxBtn": "exportToXlsx",
-                "click #top-bar-importBtn"      : "importEvent",
-                "change .inputAttach"           : "importFiles"
-            },
-
-            changeContentViewType: function (e) {
-                Custom.changeContentViewType(e, this.contentType, this.collection);
-            },
+        var TopBarView = BaseView.extend({
+            el            : '#top-bar',
+            contentType   : "Persons",
+            template      : _.template(ContentTopBarTemplate),
+            importTemplate: _.template(importTemplate),
 
             initialize: function (options) {
+                this.actionType = options.actionType;
+                if (this.actionType !== "Content") {
+                    Custom.setCurrentVT("form");
+                }
                 if (options.collection) {
                     this.collection = options.collection;
+                    this.collection.bind('reset', _.bind(this.render, this));
                 }
                 this.render();
-            },
-
-            exportToCsv: function (event) {
-                event.preventDefault();
-                this.trigger('exportToCsv');
-            },
-
-            exportToXlsx: function (event) {
-                event.preventDefault();
-                this.trigger('exportToXlsx');
-            },
-
-            createEvent: function (event) {
-                event.preventDefault();
-                this.trigger('createEvent');
-            },
-
-            render: function () {
-                $('title').text(this.contentType);
-                var viewType = Custom.getCurrentVT();
-                this.$el.html(this.template({viewType: viewType, contentType: this.contentType}));
-
-                Common.displayControlBtnsByActionType('Content', viewType);
-                return this;
-            },
-
-            editEvent: function (event) {
-                event.preventDefault();
-                this.trigger('editEvent');
-            },
-
-            importEvent: function (event) {
-                var template = _.template(importTemplate);
-                this.$el.find('#forImport').html(template);
-                event.preventDefault();
-                this.$el.find('#inputAttach').click();
-                this.trigger('importEvent');
-            },
-
-            importFiles: function (e) {
-                var importFile = new AttachView({});
-
-                this.import = true;
-
-                importFile.sendToServer(e, null, this);
-            },
-
-            deleteEvent: function (event) {
-                event.preventDefault();
-                var answer = confirm("Really DELETE items ?!");
-                if (answer === true) {
-                    this.trigger('deleteEvent');
-                }
             }
         });
 
