15ae847942c643e203d55fc0b042f519c2d504f4
umkaline <glmax132@gmail.com>
2016-02-23 17:30:40 +0200

    cosmetic fix

--- updateCollections/toDo/updateISOYearWTrack.js
+++ updateCollections/toDo/updateISOYearWTrack.js
@@ -1,26 +1,31 @@
 var mongoose = require('mongoose');
 var isoWeekYearComposer = require('../../helpers/isoWeekYearComposer');
+var dbObject;
 require('../../models/index.js');
 
-var dbObject = mongoose.createConnection('localhost', 'production');
+dbObject = mongoose.createConnection('localhost', 'production');
 
 dbObject.on('error', console.error.bind(console, 'connection error:'));
 
 dbObject.once('open', function callback() {
+    var query;
+    var wTrackSchema;
+    var wTrack;
+
     console.log("Connection to weTrack is success");
 
-    var wTrackSchema = mongoose.Schemas.wTrack;
-    var wTrack = dbObject.model("wTrack", wTrackSchema);
+    wTrackSchema = mongoose.Schemas.wTrack;
+    wTrack = dbObject.model("wTrack", wTrackSchema);
 
-    var query = wTrack.find({$or:[{month:12, week:1},{month:1, week: 53}]});
+    query = wTrack.find({$or:[{month:12, week:1},{month:1, week: 53}]});
 
-    query.exec(function (error, _res) {
+    query.exec(function (error, res) {
         if (error) {
             return console.dir(error);
         }
         var isoYear;
 
-        _res.forEach(function(wt){
+        res.forEach(function(wt){
             isoYear = isoWeekYearComposer(wt);
             wTrack.findByIdAndUpdate(wt._id, {$set: {'isoYear': isoYear}}, function(err, response){
                 if (err){
