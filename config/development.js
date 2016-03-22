d1953038ca55cc2c2d2907efafd5cab8044f58b5
romashka50 <roma.buchuk@gmail.com>
2016-03-22 08:53:51 +0000

    config changed

--- config/development.js
+++ config/development.js
@@ -1,10 +1,10 @@
 //process.env.MAIN_DOMAIN = 'testerp.com';
 process.env.HOST = 'http://localhost:8089/';
 
-process.env.MAIN_DB_HOST = 'localhost';
+process.env.MAIN_DB_HOST = '144.76.56.111';
 process.env.MAIN_DB_NAME = 'mainDB';
-process.env.DB_PORT = 27017;
-process.env.DB_USER = 'easyErp';
+process.env.DB_PORT = 28017;
+process.env.DB_USER = 'admin';
 process.env.DB_PASS = '1q2w3e!@#';
 
 process.env.OXR_APP_ID = '90432684fb4f46f483317e4ec2cd8bcc';
