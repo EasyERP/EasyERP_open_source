91a623fe960f8d57cad5b479e039820ff447bd6b
pasha <pasha.demko@gmail.com>
2016-04-05 11:57:08 +0300

    added condition for only developDepartments in dd deparments task 955

--- handlers/department.js
+++ handlers/department.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var objectId = mongoose.Types.ObjectId;
 var Department = function (models) {
     var access = require("../Modules/additions/access.js")(models);
     var DepartmentSchema = mongoose.Schemas['Department'];
@@ -12,9 +13,14 @@ var Department = function (models) {
 
     this.getForDD = function (req, res, next) {
         var Department = models.get(req.session.lastDb, 'Department', DepartmentSchema);
+        var query = req.query;
+        var matchQuery = {};
+        if (query.devDepartments){
+            matchQuery.parentDepartment = objectId('56cebdf6541812c07197358f');
+        }
 
         Department
-            .find()
+            .find(matchQuery)
             .select('_id departmentName')
             .sort({departmentName: 1})
             .lean()
