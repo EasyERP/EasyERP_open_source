1545748b40dff646b64d47338caff8e3fee0dd1e
umkaline <glmax132@gmail.com>
2016-04-11 10:44:23 +0300

    HotFix undefined employee

--- requestHandler.js
+++ requestHandler.js
@@ -742,6 +742,10 @@ var requestHandler = function (app, event, mainDb) {
 
                         empKeys.forEach(function (empId) {
                             wTRack.forEach(function (wTrack) {
+                                if (!wTrack.employee || !wTrack.employee._id) {
+                                    return;
+                                }
+
                                 var emp = (wTrack.employee._id).toString();
 
                                 nextDate = wTrack.dateByWeek;
