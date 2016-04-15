95d325feb267d79760fe2e7f47e7297615daa46a
PikinerLiliya <liliya.pikiner@gmail.com>
2016-04-15 09:23:33 +0300

    change host

--- modulesCreators/addModules(Profit and Loss).js
+++ modulesCreators/addModules(Profit and Loss).js
@@ -3,7 +3,7 @@
  */
 // JavaScript source code
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/production';
+var url = 'mongodb://erp.thinkmobiles.com:27017/production';
 //var url = 'mongodb://localhost:27017/EasyERP';
 //var url = 'mongodb://localhost:27017/testCrm';
 var async = require('async');
