bbd58ba80db912187e0dbc3456d92052c30a0196
pasha <pasha.demko@gmail.com>
2016-05-05 18:52:15 +0300

    added overdue on dueDate on salesInvoices

--- handlers/invoice.js
+++ handlers/invoice.js
@@ -1272,7 +1272,8 @@ var Invoice = function (models, event) {
                                     approved        : 1,
                                     _type           : 1,
                                     removable       : 1,
-                                    paid            : 1
+                                    paid            : 1,
+                                    diffDates       : {$cond: [{$eq : ['$workflow.status', 'Done']}, {$subtract: ['$paymentDate', '$dueDate']}, {$subtract: [new Date(), '$dueDate']}]}
                                 }
                             }, {
                                 $match: optionsObject
