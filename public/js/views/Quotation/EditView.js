e414cbca9a2ce70074fe9a39a8d614f84de2cb5d
pasha <pasha.demko@gmail.com>
2016-05-12 14:28:29 +0300

    reverted Changes

--- public/js/views/Quotation/EditView.js
+++ public/js/views/Quotation/EditView.js
@@ -551,7 +551,7 @@ define([
                 destination      : destination,
                 incoterm         : incoterm,
                 invoiceControl   : invoiceControl,
-                paymentTerms     : paymentTerm,
+                paymentTerm      : paymentTerm,
                 fiscalPosition   : fiscalPosition,
                 paymentInfo      : {
                     total  : total,
@@ -654,9 +654,8 @@ define([
 
         render: function () {
             var self = this;
-            var currentModelJSON = this.currentModel.toJSON();
             var formString = this.template({
-                model        : currentModelJSON,
+                model        : this.currentModel.toJSON(),
                 visible      : this.visible,
                 hidePrAndCust: this.hidePrAndCust
             });
@@ -706,13 +705,7 @@ define([
             populate.get("#destination", "/destination", {}, 'name', this, false, true);
             populate.get("#incoterm", "/incoterm", {}, 'name', this, false, true);
             populate.get("#invoicingControl", "/invoicingControl", {}, 'name', this, false, true);
-
-            if (!currentModelJSON.paymentTerms && currentModelJSON.project && currentModelJSON.project.paymentTerms) {
-                populate.get("#paymentTerm", "/paymentTerm", {}, 'name', this, false, true, currentModelJSON.project.paymentTerms);
-            } else {
-                populate.get("#paymentTerm", "/paymentTerm", {}, 'name', this, false, true);
-            }
-
+            populate.get("#paymentTerm", "/paymentTerm", {}, 'name', this, false, true);
             populate.get("#deliveryDd", "/deliverTo", {}, 'name', this, false, true);
 
             if (this.forSales) {
