73b914261dd608d6bf0eb115abe96269684f28a0
romashak50 <roma.buchuk@gmail.com>
2016-05-12 14:37:45 +0300

    Host change in script

--- updateCollections/toDo/renameMonthHoursToCostCenter.js
+++ updateCollections/toDo/renameMonthHoursToCostCenter.js
@@ -5,19 +5,19 @@ var async = require('async');
 var ModuleSchema = mongoose.Schemas.modules;
 
 var connectOptions = {
-    user  : 'easyerp',
+    user  : 'easyErp',
     pass  : '1q2w3e!@#',
     w     : 1,
     j     : true
 };
 
-var dbObject = mongoose.createConnection('144.76.56.111', 'pavlodb', 28017, connectOptions);
+var dbObject = mongoose.createConnection('localhost', 'production', 27017, connectOptions);
 
 //var dbObject = mongoose.createConnection('localhost', 'production');
 
 var module = dbObject.model("modules", ModuleSchema);
 
-var query = module.findByIdAndUpdate({_id : 68}, {parrent : 96, mname : 'Cost center'}, function (err) {
+module.findByIdAndUpdate({_id : 68}, {parrent : 96, mname : 'Cost center'}, function (err) {
     if (err) {
         return console.dir(err);
     }
