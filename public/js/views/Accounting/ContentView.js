a7adbf16b0929bafe59cdf785979c4bec21da7f1
pasha <pasha.demko@gmail.com>
2016-05-13 16:05:54 +0300

    changed method of render in accounting

--- public/js/views/Accounting/ContentView.js
+++ public/js/views/Accounting/ContentView.js
@@ -13,41 +13,21 @@ define([
             el         : '#content-holder',
             initialize : function (options) {
                 this.startTime = options.startTime;
+
+                this.payMethodsCollection = new payMethodsCollection();
+                this.payMethodsCollection.bind('reset', this.renderPaymentMethods, this);
                 this.render();
             },
 
-            renderPaymentMethods: function (cb) {
-                var self = this;
-
-                this.payMethodsCollection = new payMethodsCollection();
-
-                function createView() {
-                    if (cb) {
-                        cb();
-                    }
-
-                    new paymentMethodView({
-                        collection      : self.payMethodsCollection
-                    }).render();
-                }
-
-                this.payMethodsCollection.bind('reset', createView);
+            renderPaymentMethods: function () {
+                new paymentMethodView({
+                    collection: this.payMethodsCollection
+                }).render();
             },
 
+
             render: function () {
-                var self = this;
-                var parallelTasks = [this.renderPaymentMethods];
-
                 this.$el.html(this.template());
-                App.startPreload();
-                _.bindAll(this, 'renderPaymentMethods');
-
-                async.parallel(parallelTasks, function (err, result) {
-                    App.stopPreload();
-                    self.$el.append("<div id='timeRecivingDataFromServer'>Created in " + (new Date() - self.startTime) + " ms</div>");
-                });
-
-
             }
 
         });
