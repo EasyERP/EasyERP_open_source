43ea173568c7a65bbd53102e2a7d79f9233f0ead
migal <michael.vashkeba>
2016-05-27 16:00:32 +0300

    JournalEntry add extra ui unit tests and refactor

--- public/js/views/Leads/list/ListView.js
+++ public/js/views/Leads/list/ListView.js
@@ -1,161 +1,159 @@
 define([
-        'jQuery',
-        'Underscore',
-        'views/listViewBase',
-        'text!templates/Leads/list/ListHeader.html',
-        'text!templates/stages.html',
-        'views/Leads/CreateView',
-        'views/Leads/list/ListItemView',
-        'views/Leads/EditView',
-        'views/Filter/FilterView',
-        'models/LeadsModel',
-        'collections/Leads/filterCollection',
-        'common'
-    ],
+    'jQuery',
+    'Underscore',
+    'views/listViewBase',
+    'text!templates/Leads/list/ListHeader.html',
+    'text!templates/stages.html',
+    'views/Leads/CreateView',
+    'views/Leads/list/ListItemView',
+    'views/Leads/EditView',
+    'views/Filter/FilterView',
+    'models/LeadsModel',
+    'collections/Leads/filterCollection',
+    'common'
+], function ($, _, listViewBase, listTemplate, stagesTamplate, createView, ListItemView, EditView, FilterView, CurrentModel, contentCollection, common) {
+    'use strict';
 
-    function ($, _, listViewBase, listTemplate, stagesTamplate, createView, ListItemView, EditView, FilterView, CurrentModel, contentCollection, common) {
-        'use strict';
+    var LeadsListView = listViewBase.extend({
+        createView              : createView,
+        listTemplate            : listTemplate,
+        listItemView            : ListItemView,
+        contentCollection       : contentCollection,
+        filterView              : FilterView,
+        totalCollectionLengthUrl: '/leads/totalCollectionLength',
+        formUrl                 : "#easyErp/Leads/form/",
+        contentType             : 'Leads',//needs in view.prototype.changeLocationHash
 
-        var LeadsListView = listViewBase.extend({
-            createView              : createView,
-            listTemplate            : listTemplate,
-            listItemView            : ListItemView,
-            contentCollection       : contentCollection,
-            filterView              : FilterView,
-            totalCollectionLengthUrl: '/leads/totalCollectionLength',
-            formUrl                 : "#easyErp/Leads/form/",
-            contentType             : 'Leads',//needs in view.prototype.changeLocationHash
+        events: {
+            "click .list td:not(.notForm)": "goToEditDialog",
+            "click #convertToOpportunity" : "openDialog",
+            "click .stageSelect"          : "showNewSelect",
+            "click .newSelectList li"     : "chooseOption"
+        },
 
-            events: {
-                "click .list td:not(.notForm)": "goToEditDialog",
-                "click #convertToOpportunity" : "openDialog",
-                "click .stageSelect"          : "showNewSelect",
-                "click .newSelectList li"     : "chooseOption"
-            },
+        initialize: function (options) {
+            $(document).off("click");
+            this.startTime = options.startTime;
+            this.collection = options.collection;
+            _.bind(this.collection.showMore, this.collection);
+            this.parrentContentId = options.collection.parrentContentId;
+            this.stages = [];
+            this.filter = options.filter || {};
+            this.sort = options.sort;
+            this.defaultItemsNumber = this.collection.namberToShow || 100;
+            this.newCollection = options.newCollection;
+            this.deleteCounter = 0;
+            this.page = options.collection.page;
+            this.startNumber = (this.page - 1) * this.defaultItemsNumber;
 
-            initialize: function (options) {
-                $(document).off("click");
-                this.startTime = options.startTime;
-                this.collection = options.collection;
-                _.bind(this.collection.showMore, this.collection);
-                this.parrentContentId = options.collection.parrentContentId;
-                this.stages = [];
-                this.filter = options.filter||{};
-                this.sort = options.sort;
-                this.defaultItemsNumber = this.collection.namberToShow || 100;
-                this.newCollection = options.newCollection;
-                this.deleteCounter = 0;
-                this.page = options.collection.page;
-                this.startNumber = (this.page - 1) * this.defaultItemsNumber;
+            this.render();
 
-                this.render();
+            this.getTotalLength(null, this.defaultItemsNumber, this.filter);
+            this.contentCollection = contentCollection;
+        },
 
-                this.getTotalLength(null, this.defaultItemsNumber, this.filter);
-                this.contentCollection = contentCollection;
-            },
+        chooseOption: function (e) {
+            var self = this;
+            var targetElement = $(e.target).parents("td");
+            var id = targetElement.attr("id");
+            var obj = this.collection.get(id);
+            obj.set({workflow: $(e.target).attr("id")});
+            obj.save(obj.changed, {
+                headers: {
+                    mid: 39
+                },
+                patch  : true,
+                success: function () {
+                    self.showFilteredPage(self.filter, self);
+                }
+            });
 
-            chooseOption: function (e) {
-                var self = this;
-                var targetElement = $(e.target).parents("td");
-                var id = targetElement.attr("id");
-                var obj = this.collection.get(id);
-                obj.set({workflow: $(e.target).attr("id")});
-                obj.save(obj.changed, {
-                    headers: {
-                        mid: 39
-                    },
-                    patch  : true,
-                    success: function () {
-                        self.showFilteredPage(self.filter, self);
-                    }
-                });
+            this.hideNewSelect();
+            return false;
+        },
 
+        pushStages: function (stages) {
+            this.stages = stages;
+        },
+
+        openDialog: function () {
+            $("#dialog-form").dialog("open");
+        },
+
+        render: function () {
+            var self;
+            var $currentEl;
+
+            $('.ui-dialog ').remove();
+
+            self = this;
+            $currentEl = this.$el;
+
+            var itemView;
+
+            $currentEl.html('');
+            $currentEl.append(_.template(listTemplate));
+
+            var itemView = new this.listItemView({
+                collection : this.collection,
+                page       : this.page,
+                itemsNumber: this.collection.namberToShow
+            });
+
+            itemView.bind('incomingStages', this.pushStages, this);
+
+            this.renderCheckboxes();
+
+            common.populateWorkflowsList("Leads", ".drop-down-filter", "", "/Workflows", null, function (stages) {
+                self.stages = stages;
+                itemView.trigger('incomingStages', stages);
+            });
+
+            $currentEl.append(itemView.render());
+
+            this.renderCheckboxes();
+
+            this.renderFilter(self);
+
+            this.renderPagination($currentEl, this);
+
+            $currentEl.append("<div id='timeRecivingDataFromServer'>Created in " + (new Date() - this.startTime) + " ms</div>");
+        },
+
+        hideNewSelect: function () {
+            $(".newSelectList").remove();
+        },
+
+        showNewSelect: function (e) {
+            if ($(".newSelectList").is(":visible")) {
                 this.hideNewSelect();
                 return false;
-            },
-
-            pushStages: function (stages) {
-                this.stages = stages;
-            },
-
-            openDialog: function () {
-                $("#dialog-form").dialog("open");
-            },
-
-            render: function () {
-                var self;
-                var $currentEl;
-
-                $('.ui-dialog ').remove();
-
-                self = this;
-                $currentEl = this.$el;
-
-                var itemView;
-
-                $currentEl.html('');
-                $currentEl.append(_.template(listTemplate));
-
-                var itemView = new this.listItemView({
-                    collection : this.collection,
-                    page       : this.page,
-                    itemsNumber: this.collection.namberToShow
-                });
-
-                itemView.bind('incomingStages', this.pushStages, this);
-
-                this.renderCheckboxes();
-
-                common.populateWorkflowsList("Leads", ".drop-down-filter", "", "/Workflows", null, function (stages) {
-                    self.stages = stages;
-                    itemView.trigger('incomingStages', stages);
-                });
-
-                $currentEl.append(itemView.render());
-
-                this.renderCheckboxes();
-
-                this.renderFilter(self);
-
-                this.renderPagination($currentEl, this);
-
-                $currentEl.append("<div id='timeRecivingDataFromServer'>Created in " + (new Date() - this.startTime) + " ms</div>");
-            },
-
-            hideNewSelect: function () {
-                $(".newSelectList").remove();
-            },
-
-            showNewSelect: function (e) {
-                if ($(".newSelectList").is(":visible")) {
-                    this.hideNewSelect();
-                    return false;
-                } else {
-                    $(e.target).parent().append(_.template(stagesTamplate, {stagesCollection: this.stages}));
-                    return false;
-                }
-
-            },
-
-            goToEditDialog: function (e) {
-                e.preventDefault();
-                var id = $(e.target).closest('tr').data("id");
-                var model = new CurrentModel({validate: false});
-                model.urlRoot = '/Leads/form';
-                model.fetch({
-                    data   : {id: id},
-                    success: function (model) {
-                        new EditView({model: model});
-                    },
-                    error  : function () {
-                        App.render({
-                            type   : 'error',
-                            message: 'Please refresh browser'
-                        });
-                    }
-                });
+            } else {
+                $(e.target).parent().append(_.template(stagesTamplate, {stagesCollection: this.stages}));
+                return false;
             }
-        });
 
-        return LeadsListView;
+        },
+
+        goToEditDialog: function (e) {
+            e.preventDefault();
+            var id = $(e.target).closest('tr').data("id");
+            var model = new CurrentModel({validate: false});
+            model.urlRoot = '/Leads/form';
+            model.fetch({
+                data   : {id: id},
+                success: function (model) {
+                    new EditView({model: model});
+                },
+                error  : function () {
+                    App.render({
+                        type   : 'error',
+                        message: 'Please refresh browser'
+                    });
+                }
+            });
+        }
     });
+
+    return LeadsListView;
+});
