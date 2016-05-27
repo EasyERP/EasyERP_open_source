37f240e95f65d1c49087f00e21499dd25a925239
romashak50 <roma.buchuk@gmail.com>
2016-05-27 09:25:55 +0300

    Project Tests

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function (config) {
 
         logLevel: config.LOG_INFO,
 
-        autoWatch: false,
+        /*autoWatch: true,*/
 
         browsers: ['Chrome', 'Firefox', /*'Safari',*/ 'IE', 'PhantomJS'],
 
