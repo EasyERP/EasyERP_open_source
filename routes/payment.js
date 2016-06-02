09b4d8a0d280d12fc5b6d0afce12e6513fec9a73
PikinerLiliya <liliya.pikiner@gmail.com>
2016-06-02 08:10:59 +0000

    leads

--- routes/payment.js
+++ routes/payment.js
@@ -2,15 +2,22 @@
 var express = require('express');
 var router = express.Router();
 var PaymentHandler = require('../handlers/payment');
+var authStackMiddleware = require('../helpers/checkAuth');
+var MODULES = require('../constants/modules');
 
 module.exports = function (models, event) {
     var handler = new PaymentHandler(models, event);
+    var moduleId = MODULES.CUSTOMER_PAYMENTS;
+    var accessStackMiddlware = require('../helpers/access')(moduleId, models);
+
+    router.use(authStackMiddleware);
+    router.use(accessStackMiddlware);
+    
+    router.get('/:byType/', handler.getForView);
 
     router.get('/amountLeftCalc', handler.amountLeftCalc);
-    router.get('/:id', handler.getById);
     router.get('/getForProject', handler.getForProject);
     router.get('/:byType/totalCollectionLength', handler.totalCollectionLength);
-    router.get('/:byType/:viewType', handler.getForView);
     router.post('/', handler.create);
     router.post('/supplier', handler.createPayOut);
     router.post('/salary', handler.salaryPayOut);
