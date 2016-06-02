3e461ee3eed457dd43235e8ac1929c64a9606234
PikinerLiliya <liliya.pikiner@gmail.com>
2016-06-02 13:41:04 +0000

    codeStyle

--- routes/projectMember.js
+++ routes/projectMember.js
@@ -1,9 +1,17 @@
 var projectMemberHandler = require('../handlers/projectMembers');
 var express = require('express');
 var router = express.Router();
+var authStackMiddleware = require('../helpers/checkAuth');
+var MODULES = require('../constants/modules');
 
 module.exports = function (models, event) {
     var handler = new projectMemberHandler(models, event);
+
+    var moduleId = MODULES.EMPLOYEES;
+    var accessStackMiddlWare = require('../helpers/access')(moduleId, models);
+
+    router.use(authStackMiddleware);
+    router.use(accessStackMiddlWare);
 
     router.post('/', handler.create);
     router.get('/', handler.getList);
