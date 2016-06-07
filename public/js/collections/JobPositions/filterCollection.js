bc82fbe4a17503ea10efec9fe67ec4c9bdd2f0cd
pasha <pasha.demko@gmail.com>
2016-06-07 14:28:56 +0300

    refactor JobPositions

--- public/js/collections/JobPositions/filterCollection.js
+++ public/js/collections/JobPositions/filterCollection.js
@@ -47,15 +47,9 @@
                     return jopPosition;
                 });
             }
-            this.totalRecords = response.total;
-            this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+           // Parent.prototype.parse.apply(this, arguments);
 
-            this.trigger('fetchFinished', {
-                totalRecords: this.totalRecords,
-                currentPage : this.currentPage,
-                pageSize    : this.pageSize
-            });
-            return response.data;
+            return Parent.prototype.parse.apply(this, arguments);
         }
     });
     return JobPositionsCollection;
