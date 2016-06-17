f2e83debc84ab9ae6800d541fb94574b0dcfee77
PikinerLiliya <liliya.pikiner@gmail.com>
2016-06-17 16:27:59 +0300

    inventoryReport

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
             {pattern: 'test/uiSpecs/fixtures/*.test.js', included: false/*, watching: true*/},
             {pattern: 'test/uiSpecs/models/*.test.js', included: false/*, watching: true*/},
             {pattern: 'test/uiSpecs/views/login.test.js', included: false/*, watching: true*/},
-            {pattern: 'test/uiSpecs/views/user.test.js', included: false/*, watching: true*/}
+            {pattern: 'test/uiSpecs/views/inventoryReport.test.js', included: false/*, watching: true*/}
 
             // {pattern: 'test/uiSpecs/**/*.test.js', included: false/*, watching: true*/}
         ],
