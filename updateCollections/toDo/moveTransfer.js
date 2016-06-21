e13c84d38cc7ac9159a09b726206e94b106db8d5
den <denys.saranyuk@thinkmobiles.com>
2016-06-21 18:07:55 +0300

    moveTransfer script was created  v0.1

---
+++ updateCollections/toDo/moveTransfer.js
@@ -0,0 +1,71 @@
+var mongoose = require('mongoose');
+var async = require('async');
+var _ = require('lodash');
+
+require('../../models/index.js');
+
+var Employee = mongoose.Schemas.Employee;
+var Transfer = mongoose.Schemas.Transfer;
+
+var connectOptions = {
+    user: 'easyerp',
+    pass: '1q2w3e!@#',
+    w   : 1,
+    j   : true
+};
+
+var dbObject = mongoose.createConnection('144.76.56.111', 'dendb', 28017, connectOptions);
+// var dbObject = mongoose.createConnection('localhost', 'production');
+
+dbObject.on('error', console.error.bind(console, 'connection error:'));
+dbObject.once('open', function callback() {
+    var Employees = dbObject.model('Employees', Employee);
+    var Transfers = dbObject.model('Transfer', Transfer);
+
+    console.log('Connection to DB is success');
+
+    Employees.find({}, function (err, result) {
+        if (err) {
+            return console.log(err);
+        }
+
+        async.each(result, function (employee, callback) {
+            var transfer = employee.transfer;
+
+            transfer.forEach(function (tr) {
+
+                Transfers.create({
+                    date                : tr.date,
+                    status              : tr.status,
+                    department          : tr.department,
+                    isDeveloper         : tr.isDeveloper,
+                    jobPosition         : tr.jobPosition,
+                    manager             : tr.manager,
+                    weeklyScheduler     : tr.weeklyScheduler,
+                    jobType             : tr.jobType,
+                    salary              : tr.salary,
+                    info                : tr.info,
+                    employee            : employee._id,
+                    scheduledPay        : null,
+                    payrollStructureType: null
+                }, function (err, res) {
+                    if (err) {
+                        return callback(err);
+                    }
+                });
+            });
+
+            callback();
+        }, function (err) {
+            if (err) {
+                return console.dir(err);
+            }
+
+            console.dir('Good');
+        });
+    });
+});
+
+
+
+
