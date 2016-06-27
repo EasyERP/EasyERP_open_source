088dfde54ebdfce494432e3701e32202a3b0832e
PikinerLiliya <liliya.pikiner@gmail.com>
2016-06-27 10:33:54 +0300

    structure types

--- models/payRoll.js
+++ models/payRoll.js
@@ -3,17 +3,17 @@ module.exports = (function () {
     var ObjectId = mongoose.Schema.Types.ObjectId;
 
     var payRollSchema = new mongoose.Schema({
-        ID      : Number,
-        employee: {type: ObjectId, ref: 'Employees', default: null},
-        year    : Number,
-        month   : Number,
-        dataKey : Number,
-        calc    : Number,
-        paid    : Number,
-        diff    : Number,
-        type    : {type: ObjectId, ref: 'journal', default: null},
-        date    : {type: Date, default: null},
-        status  : {type: Boolean, default: false}
+        ID        : Number,
+        employee  : {type: ObjectId, ref: 'Employees', default: null},
+        year      : Number,
+        month     : Number,
+        dataKey   : Number,
+        earnings  : {type: Array, default: []},
+        deductions: {type: Array, default: []},
+        paid      : Number,
+        diff      : Number,
+        date      : {type: Date, default: null},
+        status    : {type: Boolean, default: false}
     }, {collection: 'PayRoll'});
 
     payRollSchema.set('toJSON', {virtuals: true});
