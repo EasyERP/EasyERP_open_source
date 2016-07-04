115cfd8b7046f8cc0b49fb0be6dccae0e046d372
romashak50 <roma.buchuk@gmail.com>
2016-07-04 18:46:24 +0300

    refactor login

--- public/js/views/login/LoginView.js
+++ public/js/views/login/LoginView.js
@@ -19,22 +19,31 @@ define([
         },
 
         events: {
-            'submit #loginForm'  : 'login',
-            'click .login-button': 'login',
-            'focus #ulogin'      : 'usernameFocus',
-            'focus #upass'       : 'passwordFocus',
-            'blur #ulogin'       : 'usernameFocus',
-            'blur #upass'        : 'passwordFocus',
-            'click #forgotPass'  : 'goForgotPass'
+            'submit #loginForm'     : 'login',
+            'click .login-button'   : 'login',
+            'submit #forgotPassword': 'forgotPassword',
+            'click .submitButton'   : 'forgotPassword',
+            'focus .ulogin'         : 'usernameFocus',
+            'focus #upass'          : 'passwordFocus',
+            'blur #ulogin'          : 'usernameFocus',
+            'blur #upass'           : 'passwordFocus',
+            'click #forgotPass'     : 'goForgotPass',
+            'click #backToLogin'    : 'goForgotPass'
         },
 
         goForgotPass: function (e) {
-            var forgotUrl = 'forgotPass';
+            var $currentEl = $(e.target);
+            var id = $currentEl.attr('id');
+            var $thisEl = this.$el;
+            var $loginForm = $thisEl.find('#loginForm');
+            var $forgotForm = $thisEl.find('#forgotPassword');
+
+            var title = id === 'forgotPass' ? 'Forgot Your password?' : 'Login to your account';
 
             e.preventDefault();
 
-            Backbone.history.fragment = '';
-            Backbone.history.navigate(forgotUrl, {trigger: true});
+            $loginForm.toggleClass('hidden');
+            $forgotForm.toggleClass('hidden');
         },
 
         usernameFocus: function () {
@@ -50,8 +59,10 @@ define([
             var currentDb = $thisEl.find('#dbs :selected').data('id');
             var $loginForm = $thisEl.find('#loginForm');
             var $errorContainer = $loginForm.find('.error');
-            var login = $thisEl.find('#ulogin').val() || '';
+            var login = $loginForm.find('.ulogin').val() || '';
             var pass = $thisEl.find('#upass').val() || '';
+            var $checkedEl = $thisEl.find('#switchElement');
+            var checked = $checkedEl.prop('checked');
             var err = '';
             var data;
 
@@ -59,12 +70,15 @@ define([
             App.currentDb = currentDb;
             App.weTrack = true;
 
+            $title.text(title);
+
             $loginForm.removeClass('notRegister');
 
             data = {
-                login: login,
-                pass : pass,
-                dbId : currentDb
+                login     : login,
+                pass      : pass,
+                dbId      : currentDb,
+                rememberMe: checked
             };
 
             if (!data.login || !data.pass) {
@@ -77,6 +91,54 @@ define([
             if (data.pass.length < 3) {
                 err += 'Password must be longer than 3 characters';
             }
+            if (err) {
+                $errorContainer.html(err);
+                $loginForm.addClass('notRegister');
+
+                return;
+            }
+
+            $.ajax({
+                url : '/users/login',
+                type: 'POST',
+                data: data,
+
+                success: function () {
+                    Custom.runApplication(true);
+                },
+
+                error: function () {
+                    $loginForm.addClass('notRegister');
+                    $errorContainer.text("Wrong Password or such user doesn't registered");
+                }
+            });
+        },
+
+        forgotPassword: function (event) {
+            var title = 'Forgot Your password?';
+            var $thisEl = this.$el;
+            var $title = $thisEl.find('#titleForm');
+            var currentDb = $thisEl.find('#dbs :selected').data('id');
+            var $forgotForm = $thisEl.find('#forgotPassword');
+            var login = $forgotForm.find('.ulogin').val() || '';
+            var err = '';
+            var data;
+
+            event.preventDefault();
+            App.currentDb = currentDb;
+            App.weTrack = true;
+
+            $title.text(title);
+
+            data = {
+                login: login,
+                dbId : currentDb
+            };
+
+            if (data.login.length < 3) {
+                err += 'Login must be longer than 3 characters' + '<br/>';
+            }
+
             if (err) {
                 $errorContainer.html(err);
                 $loginForm.addClass('notRegister');
@@ -115,9 +177,9 @@ define([
 
             $backStratch = $thisEl.find('#backstretch');
             $backStratch.backstretch([
-                "http://dl.dropbox.com/u/515046/www/outside.jpg"
-                , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
-                , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
+                'http://dl.dropbox.com/u/515046/www/outside.jpg',
+                'http://dl.dropbox.com/u/515046/www/garfield-interior.jpg',
+                'http://dl.dropbox.com/u/515046/www/cheers.jpg'
             ], {duration: 3000, fade: 750});
 
             return this;
