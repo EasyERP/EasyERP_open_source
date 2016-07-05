502639db18a52fd6a7437adcd299791edfdba27d
romashak50 <roma.buchuk@gmail.com>
2016-07-05 15:41:40 +0300

    config

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 //process.env.MAIN_DOMAIN = 'testerp.com';
-// process.env.HOST = 'http://erp.thinkmobiles.com/';
-process.env.HOST = 'http://testdemo.easyerp.com/';
+process.env.HOST = 'http://erp.thinkmobiles.com/';
+// process.env.HOST = 'http://testdemo.easyerp.com/';
 process.env.TRACKER_HOST = 'http://projects.thinkmobiles.com:8091';
 process.env.MAIN_DB_HOST = 'localhost';
 process.env.MAIN_DB_NAME = 'mainDB';
