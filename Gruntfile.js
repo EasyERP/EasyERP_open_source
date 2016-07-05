1dc1b06019ebcb2b897c3e3ff2c51be7be66c478
IvanKhartov <ivan.khartov@gmail.com>
2016-07-05 19:13:21 +0300

    add default typography style

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,3 +66,33 @@ module.exports = function (grunt) {
     grunt.registerTask('dist-css', ['sass', 'postcss']);
     // grunt.registerTask('scss', ['sass']);
 };
+
+// /**
+//  * http://gruntjs.com/configuring-tasks
+//  */
+// module.exports = function (grunt) {
+//     // Project configuration.
+//     grunt.initConfig({
+//         jsdoc    : {
+//             dist: {
+//                 src    : ['handlers/**/*.js', 'Modules/**/*.js', 'routes/**/*.js', 'models/**/*.js'],
+//                 options: {
+//                     destination: 'doc',
+//                     template   : "node_modules/grunt-jsdoc/node_modules/ink-docstrap/template",
+//                     configure  : "jsdoc.json"
+//                 }
+//             }
+//         },
+//         karma: {
+//             all: {
+//                 configFile: 'karma.conf.js'
+//             }
+//         }
+//     });
+//
+//     //grunt.loadNpmTasks('grunt-jsdoc');
+//     grunt.loadNpmTasks('grunt-karma');
+//
+//     //grunt.registerTask('default', ['jsdoc']);
+//     grunt.registerTask('default', ['karma']);
+// };
