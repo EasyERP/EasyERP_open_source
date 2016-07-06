4d5daa99407fe63d2b0f331bfdaf8a73c6f20fd3
Notika <natalia.androsova@thinkmobiles.com>
2016-07-06 14:49:52 +0300

    add salary chart

--- public/js/views/hrDashboard/index.js
+++ public/js/views/hrDashboard/index.js
@@ -771,7 +771,248 @@ define([
 
             });
         },
+        
+        renderSalaryChart: function(){
+            var h = 400;
+            var w = 600;
+            var padding = 15;
+            var labelPadding = 20;
 
+            /*var data = [
+             {"count":2,"source":"olga.sikora","isOpp":true},
+             {"count":5,"source":"olga.sikora","isOpp":false},
+             {"count":6,"source":"alex.vinogradov","isOpp":false},
+             {"count":4,"source":"roman.siladii","isOpp":false},
+             {"count":2,"source":"dima.lylyk","isOpp":true},
+             {"count":2,"source":"peter.volosh","isOpp":false},
+             {"count":7,"source":"alona.yelahina","isOpp":false},
+             {"count":1,"source":"roland.katona","isOpp":true},
+             {"count":1,"source":"yana.dufynets","isOpp":false},
+             {"count":1,"source":"galina.milchevych","isOpp":true},
+             {"count":5,"source":"natalia.yartysh","isOpp":false},
+             {"count":2,"source":"alina.yurenko","isOpp":true},
+             {"count":2,"source":"roland.katona","isOpp":false},
+             {"count":7,"isOpp":true},
+             {"count":1,"source":"alex.vinogradov","isOpp":true},
+             {"count":9,"source":"sergiy.biloborodov","isOpp":false},
+             {"count":2,"source":"patritsiia.danch","isOpp":false},
+             {"count":5,"source":"galina.milchevych","isOpp":false},
+             {"count":1,"source":"yevgenia.melnyk","isOpp":true},
+             {"count":37,"isOpp":false},
+             {"count":21,"source":"alina.yurenko","isOpp":false},
+             {"count":7,"source":"andriy.merentsov","isOpp":false},
+             {"count":8,"source":"yevgenia.melnyk","isOpp":false},
+             {"count":2,"source":"natalia.yartysh","isOpp":true},
+             {"count":3,"source":"larysa.popp","isOpp":false},
+             {"count":22,"source":"dima.lylyk","isOpp":false},
+             {"count":4,"source":"alina.slavska","isOpp":false},
+             {"count":9,"source":"bohdana.stets","isOpp":false},
+             {"count":1,"source":"oksana.pylyp","isOpp":true},
+             {"count":6,"source":"oksana.pylyp","isOpp":false}];*/
+
+
+            var data = {
+                "data": [120, 350, 800, 0, 0, 800, 0, 114, 114, 500, 114, 350, 350, 450, 350, 800, 800, 450, 350,
+                    300, 500, 500, 450, 250, 189, 500, 400, 114, 500, 700, 450, 400, 900, 450, 300, 350, 250, 400, 300,
+                    400, 500, 500, 350, 400, 500, 600, 1300, 900, 350, null, 700, 1700, 500, 800, 800, 1400, 1400, 1400, 250, 400, 550, 250, 500,
+                    1000, 550, 1500, 600, 350, 400, 2200, 700, 800, 0, 114, 750, 350,
+                    600, 250, 350, 450, 250, 1500, 350, 1000, 1000, 350, 250, 600, 200, 500, 1000, 1200, 120, 350, 450, 1000, 350,
+                    1100, 1200, 900, 500, 200, 500, 1400, 350, 250, 2700, 1000, 100, 700, 346, 1400, 350, 0, 350, 450, 700, 1000, 350, 600, 1100,
+                    150, 1200,1200, 700, 1000, 2200, 600, 2200, 300, 500, 350, 1600, 150, 450, 250, 500, 1000, 350, 700, 450, 250, 900, 500, 400, 450, 1200,
+                    800, 500, 350, 800, 450, 650, 600, 600, 600, 250, 550, 800, 300, 450, 300, 1000, 600, 1100, 500, 350,
+                    1200, 600, 800, 350, 550, 350,
+                    800, 450, 550, 500, 250, 100, 700, 350, 700, 650, 400, 300, 500, 550, 800, 300, 500, 700, 400, 700, 350, 600, 400, 600
+                ]
+            };
+
+            var labels = ['>=$2000', '$1750-1500', '$1500-1750', '$1250-1500', '$1000-1250', '$750-1000',
+                '$500-750', '$250-500', '<$250'];
+
+            var globalSalary = {
+                '>=$2000'   : [],
+                '':[],
+                '$1750-1500': [],
+                '$1500-1750': [],
+                '$1250-1500': [],
+                '$1000-1250': [],
+                '$750-1000' : [],
+                '$500-750'  : [],
+                '$250-500'  : [],
+                '<$250'     : []
+            };
+
+            var dataLength = data.length;
+            var arrLength = [];
+            for (var i = dataLength; i--;) {
+
+                if (data[i] >= 100000) {
+                    globalSalary['100-100'].push(data[i]);
+                } else if (data[i] === 90000) {
+                    globalSalary['90-90'].push(data[i]);
+                } else if (data[i] < 90000 && data[i] > 80000) {
+                    globalSalary['$1750-1500'].push(data[i]);
+                } else if (data[i] < 80000 && data[i] > 70000) {
+                    globalSalary['$1250-1500'].push(data[i]);
+                } else if (data[i] < 70000 && data[i] > 60000) {
+                    globalSalary['$1000-1250'].push(data[i]);
+                } else if (data[i] < 60000 && data[i] > 50000) {
+                    globalSalary['$750-1000'].push(data[i]);
+                } else if (data[i] < 50000 && data[i] > 40000) {
+                    globalSalary['$500-750'].push(data[i]);
+                } else if (data[i] < 40000 && data[i] > 30000) {
+                    globalSalary['$250-500'].push(data[i]);
+                } else {
+                    globalSalary['<$250'].push(data[i]);
+                }
+
+            }
+
+            var svg = d3.select('#wrapper')
+                .append('svg')
+                .attr({
+                    'width' : w,
+                    'height': h,
+                    'style' : 'padding: 150px'
+                });
+
+            var keys = Object.keys(globalSalary);
+
+            for (var j = keys.length; j--;) {
+                arrLength.push(globalSalary[keys[j]].length)
+            }
+
+            var maxLength = Math.max.apply(null, arrLength);
+            var step = 5;
+            var numOfGrids = Math.ceil(maxLength / 10) * 10 / step + 1;
+
+            var xGrid = d3.range(numOfGrids).map(function (i) {
+                return {
+                    'x1': 0,
+                    'y1': 0,
+                    'x2': 0,
+                    'y2': h + padding
+                };
+            });
+
+            var yGrid = d3.range(keys.length + 1).map(function(i){
+                return {
+                    'x1': - padding,
+                    'y1': 0,
+                    'x2': 0,
+                    'y2': 0
+                };
+            });
+
+            var xScale = d3.scale.linear()
+                .domain([0, Math.ceil(maxLength / 10) * 10])
+                .range([0, w]);
+
+            var yScale = d3.scale.linear()
+                .domain([0, keys.length])
+                .range([0, h]);
+
+            var grids = svg.append('g')
+                .attr('id', 'x-grid')
+                .selectAll('line')
+                .data(xGrid)
+                .enter()
+                .append('line')
+                .attr({
+                    'x1': function (d, i) {
+                        return xScale(i * step);
+                    },
+                    'y1': function (d) {
+                        return d.y1;
+                    },
+                    'x2': function (d, i) {
+                        return xScale(i * step);
+                    },
+                    'y2': function (d) {
+                        return d.y2;
+                    }
+                })
+                .style({'stroke': '#000', 'stroke-width': '1px'});
+
+            var gridsY = svg.append('g')
+                .attr('id', 'y-grid')
+                .selectAll('line')
+                .data(yGrid)
+                .enter()
+                .append('line')
+                .attr({
+                    'x1': function (d, i) {
+                        return d.x1;
+                    },
+                    'y1': function (d, i) {
+                        return yScale(i);
+                    },
+                    'x2': function (d, i) {
+                        return  d.x2;
+                    },
+                    'y2': function (d, i) {
+                        return yScale(i);
+                    }
+                })
+                .style({'stroke': '#000', 'stroke-width': '1px'});
+
+            var dy = h / (keys.length);
+            var rectWidth =  dy / 3;
+
+            svg.selectAll('rect')
+                .data(keys)
+                .enter()
+                .append('rect')
+                .attr({
+                    x     : function () {
+                        return 0;
+                    },
+                    y     : function (d, i) {
+                        return yScale(i) + rectWidth;
+                    },
+                    width : function (d, i) {
+                        return xScale(globalSalary[keys[i]].length);
+                    },
+                    height: function (d,i) {
+                        return rectWidth;
+                    },
+                    fill  : 'blue'
+                });
+
+            var xTickVal = xGrid.map(function(d,i){
+                return i*step;
+            });
+
+            var	xAxis = d3.svg.axis();
+            xAxis
+                .orient('bottom')
+                .scale(xScale)
+                .tickPadding(labelPadding)
+                .tickSize(1)
+                .tickValues(xTickVal);
+
+            var	yAxis = d3.svg.axis();
+            yAxis
+                .orient('left')
+                .scale(yScale)
+                .tickSize(0)
+                .tickPadding(padding)
+                .tickFormat(function(d, i){
+                    return labels[i];
+                })
+                .tickValues(d3.range(9));
+
+            var y_xis = svg.append('g')
+                .attr('id','yaxis')
+                .attr('transform', 'translate(0,' + dy/2 + ')')
+                .call(yAxis);
+
+            var x_xis = svg.append('g')
+                .attr("transform", 'translate(0,' + h + ')')
+                .attr('id','xaxis')
+                .call(xAxis);
+            
+        },
+        
         render: function () {
             var self = this;
             var $currentEl = this.$el;
