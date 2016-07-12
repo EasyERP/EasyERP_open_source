e27dba7c9b1ba6165b3d5653eb49adb601f9d4b9
vasyok <vasiliy.voedilo@thinkmobiles.com>
2016-07-12 15:33:24 +0300

    )

--- routes/journals.js
+++ routes/journals.js
@@ -12,8 +12,106 @@ module.exports = function (models, event) {
     router.use(authStackMiddleware);
     router.use(accessStackMiddleware);
 
+    /**
+     *@api {get} /journals/ Request Journals
+     *
+     * @apiVersion 0.0.1
+     * @apiName getJournals
+     * @apiGroup Journals
+     *
+     * @apiParam (?Field=value) {String} viewType="list" Type of View
+     * @apiParam (?Field=value) {Number} page=1 Number of page
+     * @apiParam (?Field=value) {Number} count=100 Count of journals which will show
+     * @apiParam (?Field=value) {String} contentType="journal" Type of content
+     *
+     * @apiSuccess {Object} Journals
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     {
+      "total": 26,
+      "data": [
+        {
+          "_id": "565ef6ba270f53d02ee71d65",
+          "name": "Invoice Journal",
+          "__v": 0,
+          "creditAccount": {
+            "_id": "565eb53a6aa50532e5df0be0",
+            "name": "200000 Product Sales"
+          },
+          "debitAccount": {
+            "_id": "565eb53a6aa50532e5df0bc9",
+            "name": "101200 Account Receivable"
+          },
+          "editedBy": {
+            "user": null
+          },
+          "createdBy": {
+            "date": "2015-12-02T13:48:42.607Z",
+            "user": null
+          },
+          "description": "",
+          "currency": {
+            "name": "USD"
+          },
+          "transaction": "invoice",
+          "type": "",
+          "date": "2015-12-02T13:48:42.607Z"
+        },
+        ...
+        ]
+      }
+     */
     router.get('/', _journalHandler.getForView);
+
+    /**
+     *@api {get} /journals/getForDd/ Request Journals for dropDown
+     *
+     * @apiVersion 0.0.1
+     * @apiName getJournalsForDD
+     * @apiGroup Journals
+     *
+     * @apiSuccess {Object} JournalsForDD
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+          "data": [
+            {
+              "_id": "57035ffd21f9b0c4313d414e",
+              "name": "Adjustment before invoice"
+            },
+            {
+              "_id": "56f3fac93fb451104c75a477",
+              "name": "Admin Salary Expenses"
+            },
+            ...
+          ]
+        }
+     */
     router.get('/getForDd', _journalHandler.getForDd);
+
+    /**
+     *@api {get} /journals/writeOff/ Request WriteOff
+     *
+     * @apiVersion 0.0.1
+     * @apiName getWriteOff
+     * @apiGroup Journals
+     *
+     * @apiSuccess {Object} WriteOff
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+            "data": [
+                {
+                    "_id": "57767a427134263421caa841",
+                    "name": "Write off / Overhead"
+                },
+                {
+                    "_id": "57767be5ca7bd4d021041d34",
+                    "name": "Write off / R&D"
+                }
+            ]
+        }
+     */
     router.get('/writeOff', _journalHandler.getWriteOff);
 
     /**
@@ -42,9 +140,58 @@ module.exports = function (models, event) {
     }
      */
     router.post('/', _journalHandler.create);
+
+    /**
+     *@api {patch} /journals/ Request for updating Journal
+     *
+     * @apiVersion 0.0.1
+     * @apiName updateJournal
+     * @apiGroup Journals
+     *
+     * @apiParamExample {json} Request-Example:
+     * [
+         {
+           "name": "Invoice Journal ",
+           "debitAccount": "565eb53a6aa50532e5df0bca",
+           "_id": "565ef6ba270f53d02ee71d65"
+         }
+     ]
+     *
+     * @apiSuccess {Object} Status
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     {
+        "success":"updated"
+     }
+     */
     router.patch('/', _journalHandler.putchBulk);
 
     router.delete('/:id', _journalHandler.remove);
+
+    /**
+     *@api {delete} /journals/ Request for deleting Journals
+     *
+     * @apiVersion 0.0.1
+     * @apiName deleteJournals
+     * @apiGroup Journals
+     *
+     * @apiParamExample {json} Request-Example:
+     * {
+          "contentType": "journal",
+          "ids": [
+            "56cc727e541812c07197356c",
+            "56cc72a8541812c07197356e",
+            "56cc72c4541812c071973570"
+          ]
+        }
+     * @apiSuccess {Object} Status
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+         {
+          "ok": 1,
+          "n": 3
+         }
+     */
     router.delete('/', _journalHandler.bulkRemove);
 
     return router;
