f688536938af92a82e46bd633a72f0a390dc6ae9
vasyok <vasiliy.voedilo@thinkmobiles.com>
2016-07-15 15:32:31 +0300

    )

--- routes/paymentMethod.js
+++ routes/paymentMethod.js
@@ -12,11 +12,11 @@ module.exports = function (models) {
     router.use(authStackMiddleware);
 
     /**
-     *@api {get} /payment/ Request PaymentMethods
+     *@api {get} /paymentMethod/ Request PaymentMethods
      *
      * @apiVersion 0.0.1
      * @apiName getPaymentMethods
-     * @apiGroup PaymentMethod
+     * @apiGroup Payment Method
      *
      * @apiSuccess {Object} PaymentMethods
      * @apiSuccessExample Success-Response:
@@ -46,11 +46,11 @@ module.exports = function (models) {
     router.get('/', handler.getForDd);
 
     /**
-     *@api {get} /payment/getForList Request PaymentMethodsForList
+     *@api {get} /paymentMethod/getForList Request PaymentMethodsForList
      *
      * @apiVersion 0.0.1
      * @apiName getPaymentMethodsForList
-     * @apiGroup PaymentMethod
+     * @apiGroup Payment Method
      *
      * @apiSuccess {Object} PaymentMethodsForList
      * @apiSuccessExample Success-Response:
@@ -69,9 +69,89 @@ module.exports = function (models) {
      */
     router.get('/getForList', handler.getForList);
 
+    /**
+     *@api {put} /paymentMethod/:id Request for updating PaymentMethod
+     *
+     * @apiVersion 0.0.1
+     * @apiName updatePaymentMethod
+     * @apiGroup Payment Method
+     *
+     * @apiParam {String} id Unique id of PaymentMethod
+     * @apiParamExample {json} Request-Example:
+     * {
+          "_id": "565f2e05ab70d49024242e10",
+          "name": "CASH UAH",
+          "account": "CASH UAH",
+          "currency": "USD",
+          "bank": "",
+          "owner": "CASH UAH"
+        }
+     *
+     * @apiSuccess {Object} UpdatedPaymentMethod
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+              "_id": "565f2e05ab70d49024242e10",
+              "name": "CASH UAH",
+              "account": "CASH UAH",
+              "currency": "UAH",
+              "bank": "",
+              "owner": "CASH UAH"
+            }
+     */
     router.put('/:id', accessStackMiddleware, handler.update);
+
+    /**
+     *@api {post} /paymentMethod/ Request for creating new PaymentMethod
+     *
+     * @apiVersion 0.0.1
+     * @apiName createNewPaymentMethod
+     * @apiGroup Payment Method
+     * @apiParamExample {json} Request-Example:
+     * {
+          "currency": "EUR",
+          "name": "CASH EUR",
+          "account": "",
+          "bank": ""
+        }
+     *
+     * @apiSuccess {Object} NewPaymentMethod
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+              "__v": 0,
+              "currency": "EUR",
+              "name": "CASH EUR",
+              "account": "",
+              "bank": "",
+              "_id": "5788d01e08e8dd0209d11a08",
+              "owner": ""
+            }
+     */
     router.post('/', accessStackMiddleware, handler.create);
 
+    /**
+     *@api {delete} /paymentMethod/:id Request for deleting PaymentMethod
+     *
+     * @apiVersion 0.0.1
+     * @apiName deletePaymentMethod
+     * @apiGroup Payment Method
+     *
+     * @apiParam {String} id Unique id of PaymentMethod
+     *
+     * @apiSuccess {Object} DeletedPaymentMethod
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+              "_id": "5788d01e08e8dd0209d11a08",
+              "currency": "EUR",
+              "name": "CASH EUR",
+              "account": "",
+              "bank": "",
+              "__v": 0,
+              "owner": ""
+            }
+     */
     router.delete('/:id', accessStackMiddleware, handler.remove);
 
     return router;
--- routes/paymentTerm.js
+++ routes/paymentTerm.js
@@ -79,10 +79,73 @@ module.exports = function (models) {
      ]
      */
     router.get('/getForList', handler.getForList);
-   
+
+    /**
+     *@api {put} /paymentTerm/:id Request for updating PaymentTerm
+     *
+     * @apiVersion 0.0.1
+     * @apiName updatePaymentTerm
+     * @apiGroup Payment Term
+     *
+     * @apiParam {String} id Unique id of PaymentTerm
+     * @apiParamExample {json} Request-Example:
+     * {
+          "_id": "5788d1f808e8dd0209d11a09",
+          "name": "22 Days",
+          "__v": 0
+        }
+     *
+     * @apiSuccess {Object} UpdatedPaymentTerm
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+              "_id": "5788d1f808e8dd0209d11a09",
+              "name": "22 Days",
+              "__v": 0
+            }
+     */
     router.put('/:id', handler.update);
+
+    /**
+     *@api {post} /paymentTerm/ Request for creating new PaymentTerm
+     *
+     * @apiVersion 0.0.1
+     * @apiName createPaymentTerm
+     * @apiGroup Payment Term
+     *
+     * @apiParamExample {json} Request-Example:
+     * {
+          "name": "10 Days"
+       }
+     *
+     * @apiSuccess {Object} NewPaymentTerm
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+              "__v": 0,
+              "name": "10 Days",
+              "_id": "5788d1f808e8dd0209d11a09"
+           }
+     */
     router.post('/', handler.create);
-    
+
+    /**
+     *@api {delete} /paymentTerm/:id Request for deleting PaymentTerm
+     *
+     * @apiVersion 0.0.1
+     * @apiName deletePaymentTerm
+     * @apiGroup Payment Term
+     *
+     * @apiParam {String} id Unique id of PaymentTerm
+     * @apiSuccess {Object} DeletedPaymentTerm
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+              "__v": 0,
+              "name": "10 Days",
+              "_id": "5788d1f808e8dd0209d11a09"
+           }
+     */
     router.delete('/:id', handler.remove);
 
     return router;
