a7495182e43f5f77289ab0e6b45d2b9f4bc0546f
Ihor <ihor.kuzma@thinkmobiles.com>
2016-07-18 12:19:13 +0300

    test

--- config/development.js
+++ config/development.js
@@ -12,6 +12,6 @@ process.env.OXR_APP_ID = '20cee8fa50cb4cb09bc6e54c7e5b901c';
 process.env.OXR_APP_ID = '90432684fb4f46f483317e4ec2cd8bcc';
 
 process.env.SOCKET_DB_PORT = 6379;
-process.env.SOCKET_DB_HOST = 'localhost';
-// process.env.SOCKET_DB_HOST = '144.76.56.111';
+/// process.env.SOCKET_DB_HOST = 'localhost';
+ process.env.SOCKET_DB_HOST = '144.76.56.111';
 process.env.SOCKET_DB = 9;
