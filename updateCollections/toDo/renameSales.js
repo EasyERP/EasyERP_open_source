bf4766cfdbf1d7d878ce79e41d608b6fd8b93729
pasha <pasha.demko@gmail.com>
2016-07-18 09:44:10 +0300

    added history to timeLine

--- updateCollections/toDo/renameSales.js
+++ updateCollections/toDo/renameSales.js
@@ -3,10 +3,7 @@
  */
 var mongoose = require('mongoose');
 require('../../models/index.js');
-var async = require('async');
-
 var ModuleSchema = mongoose.Schemas.modules;
-var WorkflowSchema = mongoose.Schemas.workflow;
 
 var connectOptions = {
     user: 'easyerp',
@@ -15,24 +12,14 @@ var connectOptions = {
     j   : true
 };
 
-//var dbObject = mongoose.createConnection('localhost', 'production', 27017, connectOptions);
 var dbObject = mongoose.createConnection('144.76.56.111', 'pavlodb', 28017, connectOptions);
-//var dbObject = mongoose.createConnection('localhost', 'production');
 
 var Module = dbObject.model("modules", ModuleSchema);
-var Workflow = dbObject.model("workflows", WorkflowSchema);
 
-var parallelTasks = [function (cb) {
-    Module.update({href: 'Deals'}, {href: "Opportunities", mname: "Opportunities"}, cb);
-}, function (cb) {
-    Module.update({href: 'Sales'}, {mname: "CRM"}, cb);
-},function (cb) {
-    Workflow.update({wId: 'Deals'}, {wId: "Opportunities"}, {multi : true}, cb);
-}];
-
-async.parallel(parallelTasks, function (err) {
+Module.update({href: 'Sales'}, {mname: "CRM"}, function (err) {
     if (err) {
         console.log(err);
     }
     console.log('Good');
 });
+
