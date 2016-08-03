172797b1d24a7e70ad95382860b1ffccb899d974
roman.martyniuk <roman.martyniuk@thinkmobiles.com>
2016-08-03 15:11:09 +0300

    constants

--- public/js/views/Import/ContentView.js
+++ public/js/views/Import/ContentView.js
@@ -20,8 +20,15 @@ define([
             var $thisEl = this.$el;
             var $contentBlock = $thisEl.find('#contentBlock');
             var $importProgress = $thisEl.find('#importProgress');
+            /*var progressStatuses = [
+                {name: "1", status: "1"},
+                {name: "2", status: "2"},
+                {name: "3", status: "3"}
+            ];
 
-            $importProgress.html();
+            $importProgress.html(this.importProgressTemplate({
+                import: progressStatuses
+            }));*/
         }
     });
     return ContentView;
