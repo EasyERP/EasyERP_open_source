85fe4a3685163163d317a8e48db7c91058bf9175
pasha <pasha.demko@gmail.com>
2016-08-04 17:16:06 +0300

    dateList Tasks CRM Activity

--- public/js/models/PersonsModel.js
+++ public/js/models/PersonsModel.js
@@ -48,7 +48,7 @@ define([
                     });
 
                     response.notes.forEach(function(note, index) {
-                        if (note.history && (note.history.changedField === 'Creation Date')){
+                        if (!note.name && note.history && (note.history.changedField === 'Creation Date')){
                             response.notes.splice(index, 1);
                             response.notes.unshift(note);
                             return;
