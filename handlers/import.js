6b869b09644e2d5593a05c0258b69a16ae8d47c0
migal <michael.vashkeba>
2016-08-04 09:37:04 +0300

    first step for import

--- handlers/import.js
+++ handlers/import.js
@@ -4,14 +4,57 @@ var Module = function (models) {
 
     var ImportSchema = mongoose.Schemas.Imports;
     var async = require('async');
-    var mapObject = require('../helpers/bodyMaper');
+    var mapObject = require('../public/js/constants/importMapping');
     var moment = require('../public/js/libs/moment/moment');
     var _ = require('lodash');
+
+    function toOneCase(item) {
+        item = item.toLowerCase();
+        item = item.replace(/[-+=_() !@#$%^&*`{}\[\]:;.,|\\]/g, '');
+
+        return item;
+    }
+
+    function comparing(item1, item2) {
+
+        if (toOneCase(item1) === toOneCase(item2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function splitFields(firstArr, secondArr) {
+        var result = {};
+        var isChanged;
+
+        for (var i = 0; i < secondArr.length; i++) {
+            isChanged = false;
+
+            for (var j = 0; j < firstArr.length; j++) {
+                if (comparing(secondArr[i], firstArr[j])) {
+                    result[secondArr[i]] = firstArr[j];
+                    isChanged = !isChanged;
+                    break;
+                }
+            }
+
+            if (!isChanged) {
+                result[secondArr[i]] = '';
+            }
+        }
+
+        return {
+            result: result
+        };
+    }
 
     this.getImportMapObject = function (req, res, next) {
         var ImportModel = models.get(req.session.lastDb, 'Imports', ImportSchema);
         var userId = req.session.uId;
         var importedKeyArray;
+        var personKeysArray = mapObject.customers;
+        var mappedObj;
 
         ImportModel.findOne({user: userId}, function (err, importedData) {
             if (err) {
@@ -20,7 +63,9 @@ var Module = function (models) {
 
             importedKeyArray = _.values(importedData.result);
 
-            res.status(200).send(importedKeyArray);
+            mappedObj = splitFields(personKeysArray, importedKeyArray);
+
+            res.status(200).send(mappedObj);
         });
     };
 
