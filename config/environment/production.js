fbaa2a99ccf422a3d58f04394db639ce5a4b641e
PikinerLiliya <liliya.pikiner@gmail.com>
2016-08-10 14:22:18 +0300

    config

--- config/environment/production.js
+++ config/environment/production.js
@@ -1,7 +1,7 @@
 // process.env.HOST = 'http://localhost:8089/';
-process.env.HOST = 'http://erp.thinkmobiles.com/';
+//process.env.HOST = 'http://erp.thinkmobiles.com/';
  process.env.HOST = 'http://testdemo.easyerp.com/';
-//process.env.TRACKER_HOST = 'http://projects.thinkmobiles.com:8091';
+process.env.TRACKER_HOST = 'http://projects.thinkmobiles.com:8091';
 // process.env.TRACKER_HOST = 'http://localhost:8091';
 process.env.MAIN_DB_HOST = 'localhost';
 process.env.MAIN_DB_NAME = 'mainDB';
