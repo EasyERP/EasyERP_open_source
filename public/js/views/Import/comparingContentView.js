380b233dbac4f0f71f63b319c34344b7c9f3b769
roman.martyniuk <roman.martyniuk@thinkmobiles.com>
2016-08-16 12:39:34 +0300

    change type is working!

--- public/js/views/Import/comparingContentView.js
+++ public/js/views/Import/comparingContentView.js
@@ -37,7 +37,6 @@ define([
                 self.data = data;
                 self.data.keys.reverse();
                 self.stepKeys = Object.keys(self.data.result);
-                self.stepKeys.reverse();
                 self.headerId = data.headerId;
                 self.render(self.data);
             });
@@ -58,12 +57,6 @@ define([
             $combobox.toggleClass('open');
         },
 
-        skipping: function (data) {
-            this.step++;
-            //this.mergingArray = [];
-            this.render(data);
-        },
-
         stepByStep: function (e) {
             var $thisEl = this.$el;
             var self = this;
@@ -74,9 +67,11 @@ define([
             var result;
             var url = 'importFile/merge';
             var $actions = this.$el.find('tr[data-id]');
-            var stepKey = this.stepKeys[(this.step - 1) >= 0 ? this.step - 1 : this.step];
+
+            var stepKey = this.stepKeys[this.step];
             var linkToFile;
             var linkName;
+
 
             this.isExist = null;
 
@@ -120,8 +115,11 @@ define([
                     });
                 }
             }
+
+            this.step++;
+
             if (this.step < this.stepKeys.length) {
-                this.skipping(data);
+                this.render(data);
             }
         },
 
