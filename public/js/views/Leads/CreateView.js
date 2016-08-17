2c9abe90115b049d6c0ae557a0e9b6c4ac717c3f
roman.buchuk <roma.buchuk@gmail.com>
2016-08-17 09:48:56 +0300

    lead company fixed

--- public/js/views/Leads/CreateView.js
+++ public/js/views/Leads/CreateView.js
@@ -117,56 +117,57 @@ define([
         },
 
         saveItem: function () {
+            var $thisEl = this.$el;
             var afterPage = '';
             var location = window.location.hash;
             var pageSplited = location.split('/p=')[1];
             var self = this;
             var mid = 24;
-            var name = $.trim(this.$el.find('#name').val());
-            var idCustomer = this.$el.find('#customerDd').attr('data-id');
+            var name = $.trim($thisEl.find('#name').val());
+            var idCustomer = $thisEl.find('#customerDd').attr('data-id');
             var address = {};
-            var salesPersonId = this.$el.find('#salesPerson').attr('data-id');
-            var expectedClosing = this.$el.find('#expectedClosingDate').val();
-            var dateBirth = this.$el.find('#dateBirthOnCreate').val();
-            var salesTeamId = this.$el.find('#salesTeam option:selected').val();
-            var first = $.trim(this.$el.find('#first').val());
-            var last = $.trim(this.$el.find('#last').val());
-            var tempCompany = $.trim(this.$el.find('#company').val());
-            var company = this.$el.find('#companyDd').attr('data-id');
+            var salesPersonId = $thisEl.find('#salesPerson').attr('data-id');
+            var expectedClosing = $thisEl.find('#expectedClosingDate').val();
+            var dateBirth = $thisEl.find('#dateBirthOnCreate').val();
+            var salesTeamId = $thisEl.find('#salesTeam option:selected').val();
+            var first = $.trim($thisEl.find('#first').val());
+            var last = $.trim($thisEl.find('#last').val());
+            var tempCompany = $.trim($thisEl.find('#company').val());
+            var company = $thisEl.find('#companyDd').attr('data-id');
 
             var contactName = {
                 first: first,
                 last : last
             };
-            var email = $.trim(this.$el.find('#email').val());
-            var func = $.trim(this.$el.find('#func').val());
-            var phone = $.trim(this.$el.find('#phone').val());
-            var mobile = $.trim(this.$el.find('#mobile').val());
+            var email = $.trim($thisEl.find('#email').val());
+            var func = $.trim($thisEl.find('#func').val());
+            var phone = $.trim($thisEl.find('#phone').val());
+            var mobile = $.trim($thisEl.find('#mobile').val());
 
-            var fax = $.trim(this.$el.find('#fax').val());
+            var fax = $.trim($thisEl.find('#fax').val());
             var phones = {
                 phone : phone,
                 mobile: mobile,
                 fax   : fax
             };
-            var workflow = this.$el.find('#workflowsDd').attr('data-id');
-            var priority = this.$el.find('#priorityDd').attr('data-id');
-            var internalNotes = $.trim(this.$el.find('#internalNotes').val());
-            var active = (this.$el.find('#active').is(':checked'));
-            var optout = (this.$el.find('#optout').is(':checked'));
-            var reffered = $.trim(this.$el.find('#reffered').val());
-            var skype = $.trim(this.$el.find('#skype').val());
-            var LI = $.trim(this.$el.find('#LI').val());
-            var FB = $.trim(this.$el.find('#FB').val());
+            var workflow = $thisEl.find('#workflowsDd').attr('data-id');
+            var priority = $thisEl.find('#priorityDd').attr('data-id');
+            var internalNotes = $.trim($thisEl.find('#internalNotes').val());
+            var active = ($thisEl.find('#active').is(':checked'));
+            var optout = ($thisEl.find('#optout').is(':checked'));
+            var reffered = $.trim($thisEl.find('#reffered').val());
+            var skype = $.trim($thisEl.find('#skype').val());
+            var LI = $.trim($thisEl.find('#LI').val());
+            var FB = $.trim($thisEl.find('#FB').val());
 
-            var source = this.$el.find('#sourceDd').attr('data-id');
+            var source = $thisEl.find('#sourceDd').attr('data-id');
 
             var usersId = [];
             var groupsId = [];
             var notes = [];
             var note;
 
-            var whoCanRW = this.$el.find("[name='whoCanRW']:checked").val();
+            var whoCanRW = $thisEl.find("[name='whoCanRW']:checked").val();
             if (internalNotes) {
                 note = {
                     title: '',
@@ -179,11 +180,11 @@ define([
                 afterPage = pageSplited.split('/')[1];
                 location = location.split('/p=')[0] + '/p=1' + '/' + afterPage;
             }
-            this.$el.find('._modalSelect').find('.address').each(function () {
+            $thisEl.find('._modalSelect').find('.address').each(function () {
                 var el = $(this);
                 address[el.attr('name')] = $.trim(el.val());
             });
-            this.$el.find('.groupsAndUser tr').each(function () {
+            $thisEl.find('.groupsAndUser tr').each(function () {
                 if ($(this).data('type') === 'targetUsers') {
                     usersId.push($(this).attr('data-id'));
                 }
@@ -193,33 +194,34 @@ define([
 
             });
             this.model.save({
-                name           : name,
-                skype          : skype,
-                social         : {
+                name            : name,
+                skype           : skype,
+                social          : {
                     LI: LI.replace('linkedin', '[]'),
                     FB: FB
                 },
-                dateBirth      : dateBirth,
-                company        : company,
-                campaign       : $('#campaignDd').attr('data-id'),
-                source         : source,
-                customer       : idCustomer || null,
-                address        : address,
-                salesPerson    : salesPersonId || null,
-                expectedClosing: expectedClosing,
-                salesTeam      : salesTeamId,
-                contactName    : contactName,
-                email          : email,
-                func           : func,
-                phones         : phones,
-                fax            : fax,
-                priority       : priority,
-                notes          : notes,
-                active         : active,
-                optout         : optout,
-                reffered       : reffered,
-                workflow       : workflow,
-                groups         : {
+                dateBirth       : dateBirth,
+                company         : company,
+                campaign        : $('#campaignDd').attr('data-id'),
+                source          : source,
+                customer        : idCustomer || null,
+                address         : address,
+                salesPerson     : salesPersonId || null,
+                expectedClosing : expectedClosing,
+                salesTeam       : salesTeamId,
+                contactName     : contactName,
+                email           : email,
+                func            : func,
+                phones          : phones,
+                fax             : fax,
+                priority        : priority,
+                notes           : notes,
+                active          : active,
+                optout          : optout,
+                reffered        : reffered,
+                workflow        : workflow,
+                tempCompanyField: tempCompany,
+                groups          : {
                     owner: self.$el.find('#allUsersSelect').attr('data-id') || null,
                     users: usersId,
                     group: groupsId
