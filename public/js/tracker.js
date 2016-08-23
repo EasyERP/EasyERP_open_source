4da493754ed5cca827332dbd02c94dd41df8d525
roman.buchuk <roma.buchuk@gmail.com>
2016-08-23 16:44:24 +0300

    tracker

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -3,7 +3,7 @@ define([
     'constants'
 ], function (dataService, CONSTANTS) {
     function track(data) {
-        var url = CONSTANTS.TRACKER_URL;
+        var url = CONSTANTS.TRACKER_URL + '/events';
 
         dataService.postData(url, data, function () {
 
